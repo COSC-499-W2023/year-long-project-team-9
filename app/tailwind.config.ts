15ac43342b5293cf84edf7ca135edc2493c2b0f8
Jan Dhillon <77344313+jansdhillon@users.noreply.github.com>
2023-10-24 15:55:16 -0700

    fix dark mode and add cloud fade animation

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -65,10 +65,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        'fadeIn': 'fadeIn 2s ease-out 200ms forwards',
       },
     },
   },
