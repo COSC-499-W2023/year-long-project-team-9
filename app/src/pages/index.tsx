ad29bc903bb95ab3a27013d44addc0f78c5f8a4c
Jan Dhillon <77344313+jansdhillon@users.noreply.github.com>
2023-10-24 11:31:42 -0700

    added nav item content dropdown but the image is still not working for light/dark mode

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -39,112 +39,245 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
 
+  const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
+
   return (
     <ThemeProvider
-    attribute="class"
-    defaultTheme="system"
-    enableSystem
-    disableTransitionOnChange
-  >
-    <div className="bg-background min-h-screen min-w-full font-inter flex flex-col gap-10">
-      <div className="grid p-5">
-        {/*first column with 5 sub-columns*/}
-        <div className=" grid grid-flow-col gap-2 p-5">
-          <NavigationMenu className="">
-            <NavigationMenuList>
-              <NavigationMenuItem className="grid justify-start min-h-full">
-                <Image className="" src="/logo.svg" alt="obscurus" width={50} height={50} />
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <h1 className=" text-md px-5 font-extrabold">obscurus</h1>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Features</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Getting Started</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>About</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem></NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="bg-background min-h-screen min-w-full font-inter flex flex-col gap-10">
+        <div className="grid p-5">
+          {/*first column with 5 sub-columns*/}
+          <div className=" grid grid-flow-col gap-2 p-5">
+            
+            <NavigationMenu className="">
+              <NavigationMenuList>
+                <NavigationMenuItem className="grid justify-start min-h-full">
+                  <Image
+                    className=""
+                    src="/logo.svg"
+                    alt="obscurus"
+                    width={50}
+                    height={50}
+                  />
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <h1 className=" text-md px-5 font-extrabold">obscurus</h1>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Features</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                          <a
+                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                            href="/"
+                          >
+                            {/* <Icons.logo className="h-6 w-6" /> */}
+                            <div className="mb-2 mt-4 text-lg font-medium">
+                              shadcn/ui
+                            </div>
+                            <p className="text-sm leading-tight text-muted-foreground">
+                              Beautifully designed components built with Radix
+                              UI and Tailwind CSS.
+                            </p>
+                          </a>
+                        </NavigationMenuLink>
+                      </li>
+                      <ListItem href="/docs" title="Introduction">
+                        Re-usable components built using Radix UI and Tailwind
+                        CSS.
+                      </ListItem>
+                      <ListItem href="/docs/installation" title="Installation">
+                        How to install dependencies and structure your app.
+                      </ListItem>
+                      <ListItem
+                        href="/docs/primitives/typography"
+                        title="Typography"
+                      >
+                        Styles for headings, paragraphs, lists...etc
+                      </ListItem>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                          <a
+                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                            href="/"
+                          >
+                            {/* <Icons.logo className="h-6 w-6" /> */}
+                            <div className="mb-2 mt-4 text-lg font-medium">
+                              shadcn/ui
+                            </div>
+                            <p className="text-sm leading-tight text-muted-foreground">
+                              Beautifully designed components built with Radix
+                              UI and Tailwind CSS.
+                            </p>
+                          </a>
+                        </NavigationMenuLink>
+                      </li>
+                      <ListItem href="/docs" title="Introduction">
+                        Re-usable components built using Radix UI and Tailwind
+                        CSS.
+                      </ListItem>
+                      <ListItem href="/docs/installation" title="Installation">
+                        How to install dependencies and structure your app.
+                      </ListItem>
+                      <ListItem
+                        href="/docs/primitives/typography"
+                        title="Typography"
+                      >
+                        Styles for headings, paragraphs, lists...etc
+                      </ListItem>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>About</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild>
+                          <a
+                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                            href="/"
+                          >
+                            {/* <Icons.logo className="h-6 w-6" /> */}
+                            <div className="mb-2 mt-4 text-lg font-medium">
+                              shadcn/ui
+                            </div>
+                            <p className="text-sm leading-tight text-muted-foreground">
+                              Beautifully designed components built with Radix
+                              UI and Tailwind CSS.
+                            </p>
+                          </a>
+                        </NavigationMenuLink>
+                      </li>
+                      <ListItem href="/docs" title="Introduction">
+                        Re-usable components built using Radix UI and Tailwind
+                        CSS.
+                      </ListItem>
+                      <ListItem href="/docs/installation" title="Installation">
+                        How to install dependencies and structure your app.
+                      </ListItem>
+                      <ListItem
+                        href="/docs/primitives/typography"
+                        title="Typography"
+                      >
+                        Styles for headings, paragraphs, lists...etc
+                      </ListItem>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem></NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
 
-          {/*second column with 2 sub-columns*/}
-          <div className="grid  grid-flow-col gap-2 justify-end items-center">
-            <div className="flex justify-end space-x-4">
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline" size="icon">
-                    <Sun className="" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-            <div className="grid items-center justify-end">
-              <Button variant="default">Sign In</Button>
+            {/*second column with 2 sub-columns*/}
+            <div className="grid  grid-flow-col gap-2 justify-end items-center">
+              <div className="flex justify-end space-x-4">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                      <Sun className="" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                      Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                      Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                      System
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+              <div className="grid items-center justify-end">
+                <Button variant="default">Sign In</Button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/*body*/}
-      <div className="grid grid-cols-2 items-center justify-items-center pl-16">
-        <div className="grid col-auto gap-5 w-100">
-          <h1 className="text-4xl font-extrabold">
-            Protect Privacy with a Single Click.
-          </h1>
-          <h2 className="font-semibold">
-            Securely Blur Faces in Your Videos with{" "}
-            <span className=" font-extrabold">obscurus</span>
-          </h2>
-          <div className="grid grid-cols-2">
-            <Input
-              className=" justify-self-auto"
-              type="email"
-              placeholder="Email"
-            />
-            <Button className=" justify-self-end px-auto  text-base">Get Started for Free</Button>
+        {/*body*/}
+        <div className="grid grid-cols-2 items-center justify-items-center pl-16">
+          <div className="grid col-auto gap-8 w-100">
+            <h1 className="text-4xl font-extrabold">
+              Protect Privacy with a Single Click.
+            </h1>
+            <h2 className="font-semibold">
+              Securely Blur Faces in Your Videos with{" "}
+              <span className=" font-extrabold">obscurus</span>
+            </h2>
+            <div className="grid grid-cols-2">
+              <Input className="" type="email" placeholder="Email" />
+              <Button className="sm:text-sm justify-self-center ml-24">Get Started</Button>
+            </div>
+          </div>
+          <div>
+            {/* {theme === "dark" ? (
+              <Image
+                src="/splashDark2.png"
+                alt="obscurus"
+                width={300}
+                height={300}
+              />
+            ) : ( */}
+              <Image
+                src="/splashDark2.png"
+                alt="obscurus"
+                width={300}
+                height={300}
+              />
+            {/* )} */}
           </div>
         </div>
-        <div>
-          <Image
-            className=""
-            src="/splash.svg"
-            alt="obscurus"
-            width={300}
-            height={300}
-          />
-        </div>
       </div>
-    </div>
     </ThemeProvider>
   );
+
+  
 }
