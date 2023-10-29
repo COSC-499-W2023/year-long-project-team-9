c3bac8d365caa0a4a36db57b174e300c45d935ae
jansdhillon <jandhilL@student.ubc.ca>
2023-10-29 01:29:24 -0700

    change page transition animation

--- app/front-end/src/components/layout.tsx
+++ app/front-end/src/components/layout.tsx
@@ -11,13 +11,13 @@ export default function Layout({ children }: LayoutProps) {
     <>
 
     <motion.div
-    initial={{ x: 300, opacity: 0 }}
-    animate={{ x: 0, opacity: 1 }}
-    exit={{ x: 300, opacity: 0 }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1, transition: { duration: 1 } }}
+    exit={{ opacity: 0 }}
     transition={{
       type: "spring",
-      stiffness: 260,
-      damping: 50,
+      stiffness: 460,
+      damping: 150,
     }}
     className="bg-background min-h-screen min-w-full font-inter"
   >
