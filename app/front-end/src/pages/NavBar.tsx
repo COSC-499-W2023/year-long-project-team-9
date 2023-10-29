5fa53919bdaa7bbdf41554b9984f3e8ac8203724
jansdhillon <jandhilL@student.ubc.ca>
2023-10-28 19:38:40 -0700

    change navbar items

--- app/front-end/src/pages/NavBar.tsx
+++ app/front-end/src/pages/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import {
@@ -10,35 +10,45 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { ArrowDownLeftSquare, Sun } from "lucide-react";
+import { ArrowDownLeftSquare, Sun, Moon } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
 } from "@/components/ui/dropdown-menu"; // Assuming you have these components
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import { useRouter } from "next/router";
 import useScroll from "@/components/hooks/scroll";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const NavBar = () => {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   const router = useRouter();
   const scroll = useScroll();
+  const [signedIn, setSignedIn] = useState(false);
 
   return (
-    <div className="sticky top-0 bg-gradient-to-b from-secondary to-background z-50 flex flex-column p-5 justify-between min-w-full">
+    <div className="sticky top-0 bg-gradient-to-b from-secondary to-background z-50 flex flex-column justify-between min-w-full">
       <div className="p-1">
         <NavigationMenu>
-          <NavigationMenuLink href="/">
-              <Image
-                className="min-h-full"
-                src="/logo.svg"
-                alt="obscurus"
-                width={50}
-                height={50}
-              />
+          <NavigationMenuLink href="/" className="p-5">
+            <Image
+              className="min-h-full"
+              src="/logo.svg"
+              alt="obscurus"
+              width={50}
+              height={50}
+            />
           </NavigationMenuLink>
 
           <NavigationMenuList>
@@ -47,28 +57,28 @@ const NavBar = () => {
                 obscurus
               </span>
             </NavigationMenuItem>
-            <NavigationMenuLink>
-              <div
-                className="font-semibold text-md p-5 hover:cursor-pointer"
-                onClick={() => scroll("#features")}
-              >
-                Features
-              </div>
-            </NavigationMenuLink>
             <NavigationMenuItem>
               <div
                 className="font-semibold text-md p-5 hover:cursor-pointer"
                 onClick={() => scroll("#about")}
               >
-                About
+                Create Request
               </div>
             </NavigationMenuItem>
+            <NavigationMenuLink>
+              <div
+                className="font-semibold text-md p-5 hover:cursor-pointer"
+                onClick={() => scroll("#features")}
+              >
+                My Requests
+              </div>
+            </NavigationMenuLink>
             <NavigationMenuItem>
               <div
                 className="font-bold grid grid-cols-2 gap-2 p-5 hover:cursor-pointer"
                 onClick={() => console.log("TODO: Go to Make a Request page")}
               >
-                <div>Make a Request</div>
+                <div>Upload/Record Video</div>
                 <ArrowDownLeftSquare
                   className=" justify-self-start rotate-180 mt-1"
                   size={20}
@@ -78,12 +88,13 @@ const NavBar = () => {
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-      <div className="grid grid-flow-col gap-2 justify-end items-center">
+      <div className="grid grid-flow-col gap-2 justify-end items-center pr-5 ">
         <div className="flex justify-end space-x-2">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline" size="icon">
-                <Sun className="bg-secondary" />
+                <Sun size={20} />
+
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
@@ -99,11 +110,74 @@ const NavBar = () => {
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
-        <div className="grid items-center justify-end">
-          <Link href="/SignIn">
-            <Button variant="default">Sign In</Button>
-          </Link>
-        </div>
+        {signedIn ? (
+         
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+        <Avatar onClick={() => setSignedIn(false)}>
+            <AvatarImage src="https://github.com/jansdhillon.png" />
+            <AvatarFallback>JD</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              Profile
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Billing
+              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Settings
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Keyboard shortcuts
+              <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>Email</DropdownMenuItem>
+                  <DropdownMenuItem>Message</DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>More...</DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuItem>
+              New Team
+              <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>GitHub</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuItem disabled>API</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            Log out
+            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+          
+        ) : (
+          <div className="grid items-center justify-end">
+
+              <Button variant="default" onClick={() => setSignedIn(true)}>Sign In</Button>
+
+          </div>
+        )}
       </div>
     </div>
   );
