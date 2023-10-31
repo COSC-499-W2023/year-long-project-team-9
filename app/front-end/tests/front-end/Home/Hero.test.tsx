cabf583ee55a10a65bbc3a93a3b5aef69a70bb05
jansdhillon <jandhilL@student.ubc.ca>
2023-10-30 17:38:34 -0700

    add landing page test

---
+++ app/front-end/tests/front-end/Home/Hero.test.tsx
@@ -0,0 +1,27 @@
+// Hero.test.tsx
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import Hero from "@/pages/Hero";
+import '@testing-library/jest-dom'
+
+describe("Hero Component", () => {
+  test("renders the hero component and toggles blur", () => {
+    render(<Hero />);
+
+    expect(screen.getByText(/Protect Privacy with a Single Click./i)).toBeInTheDocument();
+    expect(screen.getByText(/Securely Blur Faces in Your Videos/i)).toBeInTheDocument();
+
+    const blurredElement = document.querySelector('.opacity-0');
+    expect(blurredElement).toBeInTheDocument();
+
+    const notBlurredTab = screen.getByText("Not Blurred");
+    fireEvent.click(notBlurredTab);
+
+    expect(document.querySelector('.opacity-0')).not.toBeInTheDocument();
+
+    const blurredTab = screen.getByText("Blurred");
+    fireEvent.click(blurredTab);
+
+    expect(document.querySelector('.opacity-0')).toBeInTheDocument();
+  });
+});
