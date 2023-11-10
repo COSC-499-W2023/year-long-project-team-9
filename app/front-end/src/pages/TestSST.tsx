309fcc40de30962fd56508c7480f18c43cb9ec85
jansdhillon <jandhilL@student.ubc.ca>
2023-11-09 23:37:23 -0800

    dynamo db connected

--- app/front-end/src/pages/TestSST.tsx
+++ app/front-end/src/pages/TestSST.tsx
@@ -11,6 +11,10 @@ import {
   DynamoDBDocumentClient,
 } from "@aws-sdk/lib-dynamodb";
 import { useEffect, useState } from "react";
+<<<<<<< Updated upstream
+=======
+import { Button } from "@/components/ui/button";
+>>>>>>> Stashed changes
 
 export async function getServerSideProps() {
   const command = new PutObjectCommand({
@@ -45,6 +49,7 @@ export async function getServerSideProps() {
 
   await db.send(update);
 
+<<<<<<< Updated upstream
   return { props: { count } };
 }
 const TestSST = ({url}:{url: string}, count: string) => {
@@ -103,4 +108,69 @@ const TestSST = ({url}:{url: string}, count: string) => {
   );
 };
 
+=======
+  return { props: { count, url } };
+}
+
+const TestSST = ({ url, count }: { url: string; count: string }) => {
+  return (
+    <Layout>
+      <div>
+        <div className="grid justify-center items-center">
+          <h1 className="text-3xl font-extrabold">My Requests</h1>
+          <div>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+
+                const file = (e.target as HTMLFormElement).file.files?.[0];
+                if (!file) {
+                  console.error("No file selected");
+                  return;
+                }
+
+                const encodedFilename = encodeURIComponent(file.name);
+                const response = await fetch(url, {
+                  method: "PUT",
+                  headers: {
+                    "Content-Type": file.type,
+                    "Content-Disposition": `attachment; filename*=UTF-8''${encodedFilename}`,
+                  },
+                  body: file,
+                });
+
+                if (!response.ok) {
+                  // Handle the error situation
+                  console.error("Upload failed:", response.statusText);
+                  return;
+                }
+
+                // Assuming the PUT operation returns a URL to the uploaded object
+                const location =
+                  response.headers.get("Location") || url.split("?")[0];
+                window.location.href = location;
+                console.log("Upload successful:", location);
+              }}
+            >
+              <input
+                title="upload"
+                name="file"
+                type="file"
+                accept="image/jpeg, image/png, video/mp4"
+              />
+              <button type="submit">Upload</button>
+            </form>
+          </div>
+        </div>
+        <div className="grid justify-center items-center py-36">
+          <div>
+            The table in DyanamoDB has been updated {count} times, and does so on render.
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+>>>>>>> Stashed changes
 export default TestSST;
