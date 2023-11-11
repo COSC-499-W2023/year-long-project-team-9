afdf61f877d011e243e2622025e628e4661a175b
jansdhillon <jandhilL@student.ubc.ca>
2023-11-11 15:32:17 -0800

    init api routing with next

--- app/front-end/src/pages/TestSST.tsx
+++ app/front-end/src/pages/TestSST.tsx
@@ -11,10 +11,7 @@ import {
   DynamoDBDocumentClient,
 } from "@aws-sdk/lib-dynamodb";
 import { useEffect, useState } from "react";
-<<<<<<< Updated upstream
-=======
 import { Button } from "@/components/ui/button";
->>>>>>> Stashed changes
 
 export async function getServerSideProps() {
   const command = new PutObjectCommand({
@@ -49,75 +46,39 @@ export async function getServerSideProps() {
 
   await db.send(update);
 
-<<<<<<< Updated upstream
-  return { props: { count } };
-}
-const TestSST = ({url}:{url: string}, count: string) => {
-  return (
-    <Layout>
-      <div>
-        {count}
-        <div className="grid justify-center items-center">
-          <h1 className="text-3xl font-extrabold">My Requests</h1>
-          <div>
-            <form
-              onSubmit={async (e) => {
-                e.preventDefault();
-
-                const file = (e.target as HTMLFormElement).file.files?.[0];
-                if (!file) {
-                  console.error("No file selected");
-                  return;
-                }
-
-                const encodedFilename = encodeURIComponent(file.name);
-                const response = await fetch(url, {
-                  method: "PUT",
-                  headers: {
-                    "Content-Type": file.type,
-                    "Content-Disposition": `attachment; filename*=UTF-8''${encodedFilename}`,
-                  },
-                  body: file,
-                });
-
-                if (!response.ok) {
-                  // Handle the error situation
-                  console.error("Upload failed:", response.statusText);
-                  return;
-                }
-
-                // Assuming the PUT operation returns a URL to the uploaded object
-                const location =
-                  response.headers.get("Location") || url.split("?")[0];
-                window.location.href = location;
-                console.log("Upload successful:", location);
-              }}
-            >
-              <input
-                title="upload"
-                name="file"
-                type="file"
-                accept="image/jpeg, image/png, video/mp4"
-              />
-              <button type="submit">Upload</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
-};
-
-=======
   return { props: { count, url } };
 }
 
 const TestSST = ({ url, count }: { url: string; count: string }) => {
+
+  const [message, setMessage] = useState("Loading...");
+
+
+  useEffect(() => {
+    console.log("useEffect");
+    async function getMessage() {
+      try {
+        const response = await fetch('/api/hello');
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        const data = await response.json();
+        setMessage(data.message);
+        console.log(data);
+      } catch (error) {
+        console.error("Failed to fetch message:", error);
+      }
+    }
+  
+    getMessage();
+  }, []);
+
+  
+
   return (
     <Layout>
       <div>
         <div className="grid justify-center items-center">
-          <h1 className="text-3xl font-extrabold">My Requests</h1>
           <div>
             <form
               onSubmit={async (e) => {
@@ -167,10 +128,14 @@ const TestSST = ({ url, count }: { url: string; count: string }) => {
             The table in DyanamoDB has been updated {count} times, and does so on render.
           </div>
         </div>
+        <div className="grid justify-center items-center py-36">
+          <div>
+            {message}
+          </div>
+          </div>
       </div>
     </Layout>
   );
 };
 
->>>>>>> Stashed changes
 export default TestSST;
