e12098610807e2920b485851ae764c597689748a
Existential-Critic <soren.is@hotmail.ca>
2023-11-18 13:23:18 -0800

    Created CreateRequest3.tsx and skeleton
    See title.

---
+++ app/front-end/src/pages/CreateRequest3.tsx
@@ -0,0 +1,28 @@
+{/*Imports*/ }
+import Layout from '@/components/layout';
+import React from 'react';
+import { format } from 'date-fns';
+import { useRouter } from 'next/router';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Textarea } from '@/components/ui/textarea';
+import { Checkbox } from '@/components/ui/checkbox';
+import { AlertCircle, Plus, X, Calendar as CalendarIcon } from 'lucide-react';
+
+{/*Functions*/ }
+const CreateRequest = () => {
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
+
+{/*Export*/ }
+export default CreateRequest;
