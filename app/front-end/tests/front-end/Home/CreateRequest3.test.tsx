2450980231caca4b549615844be11dc224573d1c
Existential-Critic <soren.is@hotmail.ca>
2023-11-23 14:47:47 -0800

    Create CreateRequest3.test.tsx and add import functions
    See title.

---
+++ app/front-end/tests/front-end/Home/CreateRequest3.test.tsx
@@ -0,0 +1,7 @@
+{/*IMPORT*/}
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import CreateRequest3 from "@/pages/CreateRequest3";
+import '@testing-library/jest-dom'
+
+{/*TEST*/}
