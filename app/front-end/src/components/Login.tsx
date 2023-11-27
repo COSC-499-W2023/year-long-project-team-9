ebc193033423ce2f53602fae07bd9385d77796c7
Existential-Critic <soren.is@hotmail.ca>
2023-11-27 11:37:23 -0800

    Added proper labels to each button
    See title.

--- app/front-end/src/components/Login.tsx
+++ app/front-end/src/components/Login.tsx
@@ -85,7 +85,7 @@ const Login = () => {
                                         width={20}
                                         height={20}
                                     />
-                                    <span className="pl-2.5">Sign in with Google</span>
+                                    <span className="pl-2.5">Sign in with Microsoft</span>
                                 </Button>
                             </div>
                             <div id="facebookButton">
@@ -96,7 +96,7 @@ const Login = () => {
                                         width={20}
                                         height={20}
                                     />
-                                    <span className="pl-2.5">Sign in with Google</span>
+                                    <span className="pl-2.5">Sign in with Facebook</span>
                                 </Button>
                             </div>
                         </div>
