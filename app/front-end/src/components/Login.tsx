8b1b29fa83664547cdc9498beb097f96f270e198
Existential-Critic <soren.is@hotmail.ca>
2023-11-27 11:35:28 -0800

    Added proper logos to each button
    See title.

--- app/front-end/src/components/Login.tsx
+++ app/front-end/src/components/Login.tsx
@@ -80,8 +80,8 @@ const Login = () => {
                             <div id="microsoftButton">
                                 <Button className="h-40px min-w-min bg-white dark:bg-[#131314] border-[#747775] dark:border-[#8E918F] border-[1px] text-[#1F1F1F] dark:text-[#E3E3E3] text-sm font-roboto font-medium px-3 py-2.5 hover:bg-transparent" onClick={SignIn}>
                                     <Image
-                                        src="/Google_Logo.svg"
-                                        alt="google"
+                                        src="/Microsoft_Logo.svg"
+                                        alt="microsoft"
                                         width={20}
                                         height={20}
                                     />
@@ -91,8 +91,8 @@ const Login = () => {
                             <div id="facebookButton">
                                 <Button className="h-40px min-w-min bg-white dark:bg-[#131314] border-[#747775] dark:border-[#8E918F] border-[1px] text-[#1F1F1F] dark:text-[#E3E3E3] text-sm font-roboto font-medium px-3 py-2.5 hover:bg-transparent" onClick={SignIn}>
                                     <Image
-                                        src="/Google_Logo.svg"
-                                        alt="google"
+                                        src="/Facebook_Logo.svg"
+                                        alt="facebook"
                                         width={20}
                                         height={20}
                                     />
