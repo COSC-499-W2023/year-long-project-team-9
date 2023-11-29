e6e94ef0914bddae5a44c09e20259070fb2c898f
jansdhillon <jandhilL@student.ubc.ca>
2023-11-28 22:17:24 -0800

    try to fix cname usage

--- app/obscurus/stacks/SiteStack.ts
+++ app/obscurus/stacks/SiteStack.ts
@@ -5,37 +5,21 @@ import {
   Table,
   Service,
 } from "sst/constructs";
-import { HostedZone } from "aws-cdk-lib/aws-route53";
 
 export default function SiteStack({ stack }: StackContext) {
-  const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "public");
   const table = new Table(stack, "counter", {
     fields: { counter: "string" },
     primaryIndex: { partitionKey: "counter" },
   });
   const service = new Service(stack, "processVideo", {
     path: "./service",
-    bind: [bucket, table],
-    cdk: {
-        fargateService: {
-          circuitBreaker: { rollback: true },
-        },
-      },
+    bind: [bucket],
   });
 
   const site = new NextjsSite(stack, "site", {
     bind: [bucket, table, service],
     environment: { TABLE_NAME: table.tableName },
-    // customDomain: {
-    //   domainName: "obscurus.me",
-    //   domainAlias: "www.obscurus.me",
-    //   cdk: {
-    //     hostedZone: HostedZone.fromHostedZoneAttributes(stack, "MyZone", {
-    //       hostedZoneId: "Z09403151W7ZFKPC0YJEL",
-    //       zoneName: "obscurus.me",
-    //     }),
-    //   },
-    // },
   });
 
   stack.addOutputs({
