bbeb48ebaee423feb0a82f541471753e68211d1e
jansdhillon <jandhilL@student.ubc.ca>
2023-11-28 21:23:57 -0800

    refactor app

---
+++ app/obscurus/stacks/SiteStack.ts
@@ -0,0 +1,27 @@
+import { StackContext, NextjsSite, Bucket, Table } from "sst/constructs";
+import { HostedZone } from "aws-cdk-lib/aws-route53";
+
+export default function SiteStack({ stack }: StackContext) {
+  const bucket = new Bucket(stack, "public");
+  const table = new Table(stack, "counter", {
+    fields: { counter: "string" },
+    primaryIndex: { partitionKey: "counter" },
+  });
+
+  const site = new NextjsSite(stack, "site", {
+    bind: [bucket, table],
+    environment: { TABLE_NAME: table.tableName },
+    // customDomain: {
+    //   domainName: "obscurus.me",
+    //   domainAlias: "www.obscurus.me",
+    //   cdk: {
+    //     hostedZone: HostedZone.fromHostedZoneAttributes(stack, "MyZone", {
+    //       hostedZoneId: "Z09403151W7ZFKPC0YJEL",
+    //       zoneName: "obscurus.me",
+    //     }),
+    //   },
+    // },
+  });
+
+  stack.addOutputs({ Site: site.url });
+}
---
+++ app/obscurus/tests/front-end/Home/Hero.test.tsx
@@ -0,0 +1,28 @@
+// Hero.test.tsx
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import Hero from "@/components/Hero";
+import '@testing-library/jest-dom'
+
+describe("Hero Component", () => {
+  test("renders the hero component and toggles blur", () => {
+    render(<Hero />);
+
+
+    expect(screen.getByText(/Communicate Privately, Share Confidently./i)).toBeInTheDocument();
+    expect(screen.getByText(/Experience seamless face blurring and video delivery with/i)).toBeInTheDocument();
+
+    const blurredElement = document.querySelector('.opacity-0');
+    expect(blurredElement).toBeInTheDocument();
+
+    const notBlurredTab = screen.getByText("Not Blurred");
+    fireEvent.click(notBlurredTab);
+
+    expect(document.querySelector('.opacity-0')).not.toBeInTheDocument();
+
+    const blurredTab = screen.getByText("Blurred");
+    fireEvent.click(blurredTab);
+
+    expect(document.querySelector('.opacity-0')).toBeInTheDocument();
+  });
+});
