03ba7e54c281d97dc2bcbfeb7dfda4f595bf702e
jansdhillon <jandhilL@student.ubc.ca>
2023-12-01 16:19:36 -0800

    try with health chck?

--- app/obscurus/stacks/SiteStack.ts
+++ app/obscurus/stacks/SiteStack.ts
@@ -5,6 +5,7 @@ import {
   Table,
   Service,
 } from "sst/constructs";
+import { Duration } from "aws-cdk-lib/core";
 
 export default function SiteStack({ stack }: StackContext) {
   const bucket = new Bucket(stack, "public");
@@ -12,15 +13,21 @@ export default function SiteStack({ stack }: StackContext) {
     fields: { counter: "string" },
     primaryIndex: { partitionKey: "counter" },
   });
+
   new Service(stack, "MyService", {
     path: "./service",
     cdk: {
       container: {
-        healthCheck: undefined
-      }
-    }
+        healthCheck: {
+          command: ["CMD-SHELL", "curl -f <http://localhost/> || exit 1"],
+          interval: Duration.minutes(15),
+          retries: 10,
+          startPeriod: Duration.minutes(15),
+          timeout: Duration.minutes(15),
+        },
+      },
+    },
   });
-  
 
   const site = new NextjsSite(stack, "site", {
     bind: [bucket, table],
