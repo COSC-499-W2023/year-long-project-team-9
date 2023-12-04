4b4eb77c17a6c0c8cdfb502bf30b7dc53333173b
N0tGoingPro <b.muhammad.abu@gmail.com>
2023-12-03 20:12:51 -0800

    making demo better

--- app/obscurus/tests/front-end/Home/Dashboard.test.tsx
+++ app/obscurus/tests/front-end/Home/Dashboard.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react'
-import Dashboard from "@/pages/Dashboard";
+import AuthDemo from "@/pages/AuthDemo";
 import '@testing-library/jest-dom'
 
 {/*Testing in title in the document*/}
 test('Render Dashboard', () => {
-    render(<Dashboard/>);
+    render(<AuthDemo/>);
     const title = screen.getByTestId('welcome')
     expect(title).not.toBeVisible();
     expect(title).toBeInTheDocument();
@@ -12,7 +12,7 @@ test('Render Dashboard', () => {
 
 {/*Testing in title is Welcome!*/}
 test('Title Content', () => {
-    render(<Dashboard/>);
+    render(<AuthDemo/>);
     const titleContent = screen.getByText('Welcome!')
     expect(titleContent).not.toBeVisible();
     expect(titleContent).toBeInTheDocument();
@@ -20,7 +20,15 @@ test('Title Content', () => {
 
 {/*Testing in link is correct*/}
 test('Title Content', () => {
-    render(<Dashboard/>);
+    render(<AuthDemo/>);
     const link = screen.getByTestId('link')
-    expect(link).toHaveAttribute('href', 'https://obscurususerpool.auth.us-west-2.amazoncognito.com/login?client_id=45sq39c3d2srgg5cm5iclt82m6&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FDashboard');
+    expect(link).toHaveAttribute('href', 'https://obscurususerpool.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=45sq39c3d2srgg5cm5iclt82m6&redirect_uri=http://localhost:3000/AuthDemo');
+});
+
+{/*Testing if the sign in button is showing*/}
+test('Correct Button', () => {
+    render(<AuthDemo/>);
+    const signInButton = screen.getByText('Sign In')
+    expect(signInButton).not.toBeVisible();
+    expect(signInButton).toBeInTheDocument();
 });
