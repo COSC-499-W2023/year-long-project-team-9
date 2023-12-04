aaf9cc802c9f6a7471bb842301db5ad238c6f38c
asiv23 <ansivana@gmail.com>
2023-12-03 18:08:29 -0800

    create tests for request listings page

---
+++ app/obscurus/tests/front-end/Home/RequestListings.test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import RequestListing from "@/pages/MyRequests";
+
+jest.mock("aos", () => ({
+  init: jest.fn(), // Mock AOS initialization
+}));
+
+describe("Features Component", () => {
+  beforeEach(() => {
+    render(<RequestListing />);
+  });
+
+  test("Render page and check features.",() => {
+    //check title
+    expect(screen.getByText("Create Request")).toBeInTheDocument();
+    //check searchbar 
+    expect(screen.getByText(/search/i)).toBeInTheDocument();
+    //check sorting options
+    expect(screen.getByText("Oldest")).toBeInTheDocument();
+    expect(screen.getByText("Edited")).toBeInTheDocument();
+    expect(screen.getByText("Overdue")).toBeInTheDocument();
+    expect(screen.getByText("Completed")).toBeInTheDocument();
+    //checks for request listings
+    expect(screen.getByText(/spanish lessons/i)).toBeInTheDocument();
+    expect(screen.getByText(/spanish lessons/i)).toBeInTheDocument();
+});
+
+});
