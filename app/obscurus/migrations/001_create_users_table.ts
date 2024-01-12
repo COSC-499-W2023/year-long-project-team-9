31871645065eda7a9d4d03d58641f1352c57c8e4
Existential-Critic <soren.is@hotmail.ca>
2024-01-12 15:55:55 -0800

    Reverted user table to old version

--- app/obscurus/migrations/001_create_users_table.ts
+++ app/obscurus/migrations/001_create_users_table.ts
@@ -2,18 +2,12 @@ import { Kysely } from "kysely";
 
 export async function up(db:Kysely<any>) {
     await db.schema
-    .createTable("Users")
+    .createTable("users")
     .addColumn("sub","text",(col) => col.primaryKey())
     .addColumn("email","text",(col) => col.notNull())
-    .addColumn("first_name","text",(col) => col.notNull())
-    .addColumn("last_name","text",(col) => col.notNull())
-    .addColumn("bday","text",(col) => col.notNull())
-    .addColumn("requests","text",(col) => col.notNull())
-    .addColumn("time_zone","text",(col) => col.notNull())
-    .addColumn("language","text",(col) => col.notNull())
     .execute();
 }
 
 export async function down(db:Kysely<any>) {
-    await db.schema.dropTable("user").execute();
+    await db.schema.dropTable("users").execute();
 }
