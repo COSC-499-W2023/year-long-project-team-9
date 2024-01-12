04e87e44bf8c60b7f3fd8e7777515f87705dc65a
Existential-Critic <soren.is@hotmail.ca>
2024-01-12 15:52:09 -0800

    Updated users table
    See title.

--- app/obscurus/migrations/001_create_users_table.ts
+++ app/obscurus/migrations/001_create_users_table.ts
@@ -2,9 +2,15 @@ import { Kysely } from "kysely";
 
 export async function up(db:Kysely<any>) {
     await db.schema
-    .createTable("user")
+    .createTable("Users")
     .addColumn("sub","text",(col) => col.primaryKey())
     .addColumn("email","text",(col) => col.notNull())
+    .addColumn("first_name","text",(col) => col.notNull())
+    .addColumn("last_name","text",(col) => col.notNull())
+    .addColumn("bday","text",(col) => col.notNull())
+    .addColumn("requests","text",(col) => col.notNull())
+    .addColumn("time_zone","text",(col) => col.notNull())
+    .addColumn("language","text",(col) => col.notNull())
     .execute();
 }
 
