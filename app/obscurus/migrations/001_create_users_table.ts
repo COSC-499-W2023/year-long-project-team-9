e616e6ebe59c3c9091d3757388bdcf7103107741
Existential-Critic <soren.is@hotmail.ca>
2024-01-12 15:25:33 -0800

    Fixed dummy table in 001_create_users_table.ts
    See title.

--- app/obscurus/migrations/001_create_users_table.ts
+++ app/obscurus/migrations/001_create_users_table.ts
@@ -1,14 +1,13 @@
 import { Kysely } from "kysely";
 
-async function up(db:Kysely<any>) {
+export async function up(db:Kysely<any>) {
     await db.schema
     .createTable("user")
     .addColumn("sub","text",(col) => col.primaryKey())
+    .addColumn("email","text",(col) => col.notNull())
     .execute();
 }
 
-async function down(db:Kysely<any>) {
+export async function down(db:Kysely<any>) {
     await db.schema.dropTable("user").execute();
 }
-
-module.exports = { up,down }
