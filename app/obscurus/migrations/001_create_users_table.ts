0e88db4a6671ff91f515b5c3643338700436916e
Existential-Critic <soren.is@hotmail.ca>
2024-01-12 14:58:47 -0800

    Added placeholder user table in migrations folder
    See title.

--- app/obscurus/migrations/001_create_users_table.ts
+++ app/obscurus/migrations/001_create_users_table.ts
@@ -1,1 +1,14 @@
 import { Kysely } from "kysely";
+
+async function up(db:Kysely<any>) {
+    await db.schema
+    .createTable("user")
+    .addColumn("sub","text",(col) => col.primaryKey())
+    .execute();
+}
+
+async function down(db:Kysely<any>) {
+    await db.schema.dropTable("user").execute();
+}
+
+module.exports = { up,down }
