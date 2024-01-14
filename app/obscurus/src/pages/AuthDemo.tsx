8438bd41fc5eb0eea6ffbd55b8706f175ddd8ea8
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-01-14 09:41:32 -0800

    fixing format of authDemo.tsx

--- app/obscurus/src/pages/AuthDemo.tsx
+++ app/obscurus/src/pages/AuthDemo.tsx
@@ -1,37 +1,49 @@
-'use client'
 import Layout from "@/components/layout";
-import { Button } from "@/components/ui/button"
-import { useSearchParams } from 'next/navigation'
+import { Button } from "@/components/ui/button";
+import { useSearchParams } from "next/navigation";
+import { redirect } from "next/navigation";
 
-const Dashboard = () => {
+const AuthDemo = () => {
+  {
+    /*I know this sucks, but it will work for now*/
+  }
 
-    {/*I know this sucks, but it will work for now*/}
-    const searchParams = useSearchParams()
-    let codePresent = false;
-    try {
-        const code = searchParams.get('code')
-        if(code != null) {
-            codePresent = true;
-        } else {
-            codePresent = false;
-        }
+  const searchParams = useSearchParams();
+  let codePresent = false;
+  try {
+    const code = searchParams.get("code");
+    if (code != null) {
+      codePresent = true;
+    } else {
+      codePresent = false;
     }
-    catch(e){
+  } catch (e) {}
 
-    }    
+  return (
+    <Layout>
+      <div className="grid justify-center items-center pt-12">
+        <p className="uppercase font-bold text-2xl pb-8" data-testid="welcome">
+          Welcome!
+        </p>
+        <a
+          data-testid="link"
+          href="https://obscuruslogin.auth.us-west-2.amazoncognito.com/login?client_id=4fh382tdlodqdcoi0c710fhv7k&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2FAuthDemo"
+        >
+          <div>
+            {codePresent ? (
+              <Button type="button" className="w-full">
+                Sign Out
+              </Button>
+            ) : (
+              <Button type="button" className="w-full">
+                Sign In
+              </Button>
+            )}
+          </div>
+        </a>
+      </div>
+    </Layout>
+  );
+};
 
-    return (
-        <Layout>    
-            <div className='grid justify-center items-center pt-12'>
-                <p className='uppercase font-bold text-2xl pb-8' data-testid="welcome">Welcome!</p>
-                <a  data-testid="link" href="https://obscurususerpool.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=45sq39c3d2srgg5cm5iclt82m6&redirect_uri=http://localhost:3000/AuthDemo">
-                    <div>
-                        {codePresent ? <Button type="button" className='w-full'>Sign Out</Button> : <Button type="button" className='w-full'>Sign In</Button>}
-                    </div>
-                </a>
-            </div>
-        </Layout>
-    )
-}
-
-export default Dashboard;
+export default AuthDemo;
