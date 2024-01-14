f798647bff628ee4c6f5454457237dfecce1735a
Existential-Critic <soren.is@hotmail.ca>
2024-01-13 16:33:41 -0800

    Added unique clause to user email in users table
    See title.

--- app/obscurus/migrations/001_users.mjs
+++ app/obscurus/migrations/001_users.mjs
@@ -7,7 +7,7 @@ export async function up(db) {
     await db.schema
     .createTable("users")
     .addColumn("sub","varchar",(col) => col.primaryKey())
-    .addColumn("email","varchar",(col) => col.notNull())
+    .addColumn("email","varchar",(col) => col.notNull().unique())
     .addColumn("given_name","varchar",(col) => col.notNull())
     .addColumn("family_name","varchar",(col) => col.notNull())
     .addColumn("birthday","date",(col) => col.notNull())
