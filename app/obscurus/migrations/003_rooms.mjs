31f83dc58c57bed9ac33e6b78de479b93de2bc2d
Existential-Critic <soren.is@hotmail.ca>
2024-01-13 16:07:28 -0800

    Added a column for participants_subs in the rooms table
    See title.

--- app/obscurus/migrations/003_rooms.mjs
+++ app/obscurus/migrations/003_rooms.mjs
@@ -10,6 +10,7 @@ export async function up(db) {
     .addColumn("room_name","varchar",(col) => col.notNull())
     .addColumn("connection_id","varchar",(col) => col.notNull())
     .addColumn("number_of_participants","integer",(col) => col.notNull())
+    .addColumn("participants_subs","varchar",(col) => col.notNull())
     .addColumn("creation_date","timestamp",(col) => col.notNull().defaultTo("now()"))
     .execute();
 }
