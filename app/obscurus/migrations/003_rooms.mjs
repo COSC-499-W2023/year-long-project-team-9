8e1039d8a83f8e78e539dea5d26db3e921c535ef
Existential-Critic <soren.is@hotmail.ca>
2024-01-13 16:02:44 -0800

    Added columns for rooms table in 003_rooms.mjs
    See title.

---
+++ app/obscurus/migrations/003_rooms.mjs
@@ -0,0 +1,22 @@
+import { Kysely } from "kysely";
+
+/**
+ * @param {Kysely<any>} db 
+ */
+export async function up(db) {
+    await db.schema
+    .createTable("rooms")
+    .addColumn("room_id","bigserial",(col) => col.primaryKey())
+    .addColumn("room_name","varchar",(col) => col.notNull())
+    .addColumn("connection_id","varchar",(col) => col.notNull())
+    .addColumn("number_of_participants","integer",(col) => col.notNull())
+    .addColumn("creation_date","timestamp",(col) => col.notNull().defaultTo("now()"))
+    .execute();
+}
+
+/**
+ * @param {Kysely<any>} db 
+ */
+export async function down(db) {
+    await db.schema.dropTable("rooms").execute();
+}
