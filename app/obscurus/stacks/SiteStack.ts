f0c653d8353b021d18fa6cc2a32d146af50dcce1
Existential-Critic <soren.is@hotmail.ca>
2024-01-14 16:38:18 -0800

    Fixed structure of files so migrations are in stacks/core/migrations
    See title.

--- app/obscurus/stacks/SiteStack.ts
+++ app/obscurus/stacks/SiteStack.ts
@@ -17,7 +17,7 @@ export default function SiteStack({ stack }: StackContext) {
   const rds = new RDS(stack, "Database", {
     engine: "postgresql11.13",
     defaultDatabaseName: "obscurus",
-    migrations: "./migrations/",
+    migrations: "./stacks/core/migrations/",
   });
 
   const site = new NextjsSite(stack, "site", {
