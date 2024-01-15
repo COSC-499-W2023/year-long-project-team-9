60b37413f5cb514e55c76b2901f40f21c4eaa459
Existential-Critic <soren.is@hotmail.ca>
2024-01-14 18:13:01 -0800

    Added API call to TestDatabase.tsx
    See title.

--- app/obscurus/src/pages/TestDatabase.tsx
+++ app/obscurus/src/pages/TestDatabase.tsx
@@ -6,41 +6,35 @@ import {Api } from "sst/node/api"
 
 {/*FUNCTIONS*/}
 export async function getServerSideProps() {
-
-
-const fetchData = async () => {
-  try {
-    const apiURL = Api.Api.url
-    console.log(Api.Api)
-    const response = await fetch(apiURL+  "/users");
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    
-    const data = await response.json();
-    console.log("Data from /users:", data);
-    // Process the data as needed
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
-
-fetchData();
-
-
+    const fetchData = async () => {
+        try {
+            const apiURL = Api.Api.url
+            const response = await fetch(apiURL+  "/users");
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            
+            const data = await response.json();
+            // Process the data as needed
+            return data;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+    const usersJSON = await fetchData();
+    console.log(usersJSON)
     return {
-      props: {}, // nothing yet
+      props: {usersJSON}, // nothing yet
     };
 };
-
-
-
-
-const TestDatabase = () => {
+const TestDatabase = ({usersJSON}:{usersJSON: any}) => {
     return (
         <Layout>
             <div className="grid justify-center items-center pt-10 ">
                 <h1 className="text-3xl font-extrabold">Database Testing</h1>
+            </div>
+            <div className="">
+
             </div>
         </Layout>
     );
