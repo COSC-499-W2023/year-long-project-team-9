98e4ecc4bcbc82e2755852aa7a5ca6ffef5154a9
Existential-Critic <soren.is@hotmail.ca>
2024-01-18 13:56:59 -0800

    Changed room table schema to fit new design
    See title.

--- app/obscurus/stacks/core/migrations/003_rooms.mjs
+++ app/obscurus/stacks/core/migrations/003_rooms.mjs
@@ -7,11 +7,13 @@ export async function up(db) {
     await db.schema
     .createTable("rooms")
     .addColumn("room_id","bigserial",(col) => col.primaryKey())
-    .addColumn("request_id","bigserial",(col) => col.notNull().references("requests.request_id"))
-    .addColumn("room_name","varchar",(col) => col.notNull())
     .addColumn("connection_id","varchar",(col) => col.notNull())
-    .addColumn("number_of_participants","integer",(col) => col.notNull())
-    .addColumn("participants_subs","varchar",(col) => col.notNull())
+    .addColumn("participant1_email","varchar",(col) => col.notNull())
+    .addColumn("participant1_sub","varchar")
+    .addColumn("participant2_email","varchar",(col) => col.notNull())
+    .addColumn("participant2_sub","varchar")
+    .addColumn("participant1_room_name","varchar",(col) => col.notNull())
+    .addColumn("participant2_room_name","varchar",(col) => col.notNull())
     .addColumn("creation_date","timestamp",(col) => col.notNull().defaultTo("now()"))
     .execute();
 }
