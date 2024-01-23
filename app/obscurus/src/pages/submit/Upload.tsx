fcef97dda8ce7e0107fd70305e5adddc3605254a
jansdhillon <jandhilL@student.ubc.ca>
2024-01-22 23:51:08 -0800

    make request dynamic from json

--- app/obscurus/src/pages/submit/Upload.tsx
+++ app/obscurus/src/pages/submit/Upload.tsx
@@ -11,7 +11,13 @@ import Link from "next/link";
 import { useCurrentTheme } from "@/components/hooks/useCurrentTheme";
 import Webcam from "react-webcam";
 import router from "next/router";
-import { Circle, Pause, ArrowBigLeft, ArrowBigRight } from "lucide-react";
+import {
+  Circle,
+  Pause,
+  ArrowBigLeft,
+  ArrowBigRight,
+  Square,
+} from "lucide-react";
 import NestedLayout from "@/components/nested-layout";
 import {
   Pagination,
@@ -22,6 +28,8 @@ import {
   PaginationEllipsis,
   PaginationNext,
 } from "@/components/ui/pagination";
+import { Card } from "@/components/ui/card";
+import { FadeLoader } from "react-spinners";
 
 export async function getServerSideProps() {
   const command = new PutObjectCommand({
@@ -67,8 +75,8 @@ export const Upload = ({ url }: { url: string }) => {
       return;
     }
 
-    const location = response.headers.get("Location") || url.split("?")[0];
-    window.location.href = location;
+    // const location = response.headers.get("Location") || url.split("?")[0];
+    // window.location.href = location;
     console.log("Upload successful:", location);
   };
 
@@ -98,8 +106,8 @@ export const Upload = ({ url }: { url: string }) => {
   };
 
   const handleStopCaptureClick = () => {
-    mediaRecorderRef.current!.stop();
     setCapturing(false);
+    mediaRecorderRef.current?.stop();
   };
 
   const handleSaveAndUpload = async () => {
@@ -112,18 +120,20 @@ export const Upload = ({ url }: { url: string }) => {
       });
 
       try {
+        const encodedFilename = encodeURIComponent(file.name);
         const response = await fetch(url, {
           method: "PUT",
           headers: {
             "Content-Type": "video/webm",
+            "Content-Disposition": `attachment; filename*=UTF-8''${encodedFilename}`,
           },
           body: file,
         });
 
         if (response.ok) {
-          const location =
-            response.headers.get("Location") || url.split("?")[0];
-          window.location.href = location;
+          // const location =
+          //   response.headers.get("Location") || url.split("?")[0];
+          // window.location.href = location;
           console.log("Upload successful:", location);
         } else {
           console.error("Upload failed:", response.statusText);
@@ -140,35 +150,47 @@ export const Upload = ({ url }: { url: string }) => {
 
   return (
     <div className="p-3">
-      <div className="text-2xl font-bold text-center py-5">Upload or Record Your Video</div>
-      {!record ? (
-        <>
-          <div className="grid justify-items-center border-dashed  border-accent border-4 rounded-lg bg-secondary dark:border-white">
-            <form onSubmit={handleSubmit} className="">
-              <div className="grid w-full gap-10 h-full  max-w-sm items-center rounded-xl p-10">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  shape-rendering="geometricPrecision"
-                  text-rendering="geometricPrecision"
-                  image-rendering="optimizeQuality"
-                  viewBox="0 0 512 364.92"
-                  width={150}
-                  fill={primary}
-                  className="justify-self-center"
+      <div className="text-2xl font-bold text-center py-5">
+        Upload or Record Your Video
+        <div> </div>
+      </div>
+
+      <Card className="drop-shadow-md border-2 border-accent bg-card w-full mt-5">
+        <form onSubmit={handleSubmit} className="">
+          {!record ? (
+            <div className="grid w-full gap-10 h-full  max-w-sm items-center rounded-xl p-10 justify-end">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                shape-rendering="geometricPrecision"
+                text-rendering="geometricPrecision"
+                image-rendering="optimizeQuality"
+                viewBox="0 0 512 364.92"
+                width={136}
+                fill={primary}
+                className="justify-self-center"
+              >
+                <path
+                  fill-rule="nonzero"
+                  d="M359.34 124.66c-13.56 6.7-25.71 15.36-37.41 24.9l-20.13-23.03c14.81-13.74 32.08-24.49 50.64-32.39-37.56-66.54-128.4-83.94-188.3-37.42-21.75 16.84-38.26 42.04-44.61 75.42l-2 10.43-10.4 1.84c-10.19 1.78-19.29 4.24-27.27 7.36-49.8 19.27-63.78 74.7-33.31 116.79 13.04 17.91 29.66 36.17 51.47 39.21h30.49c-.21 3-.32 6.02-.32 9.07 0 7.34.62 14.53 1.82 21.53H97.53l-1.92-.17c-30.53-3.88-55.62-26.61-73.75-51.72C-20.41 228.28.4 149.83 68.93 123.25c7.15-2.79 14.8-5.12 22.86-6.99 9.15-36.34 28.65-64.32 53.72-83.74C222.7-27.29 338.58-1.8 382.79 86.08c6.65-1.05 13.31-1.58 19.92-1.5 98.9.73 138.01 127.2 86.69 195.33-20.57 27.29-52.12 50.51-84.61 58.05l-3.41.41h-19.81a128.362 128.362 0 0 0 1.5-30.6h16.61c24.15-5.81 49.91-25.68 65.23-46.16 36.46-48.56 10.58-146.05-62.41-146.51-14.26-.12-29.11 3.33-43.16 9.56zM234.73 364.92h42.56c9.95 0 18.11-8.16 18.11-18.11v-58.64h31.04c6.54-.28 11.18-2.44 13.87-6.52 7.27-10.91-2.66-21.68-9.55-29.27-19.55-21.46-53.22-53.23-62.87-64.59-7.32-8.08-17.72-8.08-25.04 0-9.97 11.64-44.84 45.77-63.43 66.64-6.45 7.27-14.42 17.17-7.72 27.22 2.76 4.08 7.35 6.24 13.89 6.52h31.04v58.64c0 9.84 8.15 18.11 18.1 18.11z"
+                />
+              </svg>
+              <div className="flex justify-center w-full">
+                <Label
+                  htmlFor="video"
+                  className="text-xl font-bold text-center"
                 >
-                  <path
-                    fill-rule="nonzero"
-                    d="M359.34 124.66c-13.56 6.7-25.71 15.36-37.41 24.9l-20.13-23.03c14.81-13.74 32.08-24.49 50.64-32.39-37.56-66.54-128.4-83.94-188.3-37.42-21.75 16.84-38.26 42.04-44.61 75.42l-2 10.43-10.4 1.84c-10.19 1.78-19.29 4.24-27.27 7.36-49.8 19.27-63.78 74.7-33.31 116.79 13.04 17.91 29.66 36.17 51.47 39.21h30.49c-.21 3-.32 6.02-.32 9.07 0 7.34.62 14.53 1.82 21.53H97.53l-1.92-.17c-30.53-3.88-55.62-26.61-73.75-51.72C-20.41 228.28.4 149.83 68.93 123.25c7.15-2.79 14.8-5.12 22.86-6.99 9.15-36.34 28.65-64.32 53.72-83.74C222.7-27.29 338.58-1.8 382.79 86.08c6.65-1.05 13.31-1.58 19.92-1.5 98.9.73 138.01 127.2 86.69 195.33-20.57 27.29-52.12 50.51-84.61 58.05l-3.41.41h-19.81a128.362 128.362 0 0 0 1.5-30.6h16.61c24.15-5.81 49.91-25.68 65.23-46.16 36.46-48.56 10.58-146.05-62.41-146.51-14.26-.12-29.11 3.33-43.16 9.56zM234.73 364.92h42.56c9.95 0 18.11-8.16 18.11-18.11v-58.64h31.04c6.54-.28 11.18-2.44 13.87-6.52 7.27-10.91-2.66-21.68-9.55-29.27-19.55-21.46-53.22-53.23-62.87-64.59-7.32-8.08-17.72-8.08-25.04 0-9.97 11.64-44.84 45.77-63.43 66.64-6.45 7.27-14.42 17.17-7.72 27.22 2.76 4.08 7.35 6.24 13.89 6.52h31.04v58.64c0 9.84 8.15 18.11 18.1 18.11z"
-                  />
-                </svg>
-                <div className="flex justify-center w-full">
-                  <Label
-                    htmlFor="video"
-                    className="text-xl font-bold text-center"
-                  >
-                    {file ? `Selected file: ${file.name}` : "No file selected."}
-                  </Label>
-                </div>
+                  {file ? `Selected file: ${file.name}` : "No file selected."}
+                </Label>
+              </div>
+              <input
+                id="file-input"
+                type="file"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={handleSubmit}
+              />
+
+              <div className="grid grid-cols-2 gap-10">
                 <input
                   id="file-input"
                   type="file"
@@ -177,77 +199,73 @@ export const Upload = ({ url }: { url: string }) => {
                   onChange={handleSubmit}
                 />
 
-                <div className="grid grid-cols-2 gap-10">
-                  <input
-                    id="file-input"
-                    type="file"
-                    ref={fileInputRef}
-                    style={{ display: "none" }}
-                    onChange={handleSubmit}
-                  />
+                <div className="justify-self-start">
+                  <Button
+                    className=" font-extrabold"
+                    onClick={handleUploadClick} // Trigger hidden file input
+                  >
+                    Upload
+                  </Button>
+                </div>
+                <div className="justify-self-end">
+                  <Button
+                    className=" font-extrabold"
+                    onClick={() => setRecord(true)}
+                  >
+                    Record
+                  </Button>
+                </div>
+              </div>
+              <div className="grid items-center text-sm justify-items-center w-full">
+                <div className="font-semibold text-base">
+                  Accepted filetypes:
+                </div>
+                <div className="text-sm text-center"> MP4, MOV</div>
+              </div>
+            </div>
+          ) : (
+            <div className=" p-4">
+                <Webcam
+                  audio={false}
+                  ref={webcamRef}
+                  className=" rounded-md "
+                />
+                <FadeLoader loading={false}/>
 
-                  <div className="justify-self-start">
+              <div className="flex flex-row justify-evenly p-3">
+                {capturing ? (
+                  <>
+                    <button onClick={handleStopCaptureClick}>
+                      <Pause />
+                    </button>
                     <Button
-                      className=" font-extrabold"
-                      onClick={handleUploadClick} // Trigger hidden file input
+                      onClick={handleSaveAndUpload}
+                      disabled={!recordedChunks.length}
                     >
                       Upload
                     </Button>
-                  </div>
-                  <div className="justify-self-end">
-                    <Button
-                      className=" font-extrabold"
-                      onClick={() => setRecord(true)}
+                  </>
+                ) : (
+                  <>
+                    <button onClick={handleStartCaptureClick}>
+                      <Circle className="fill-red-500 stroke-none" />
+                    </button>
+                    <button onClick={() => setRecord(false)}>
+                      <Square fill={primary} />
+                    </button>
+                    <button
+                      onClick={handleSaveAndUpload}
+                      disabled={!recordedChunks.length}
                     >
-                      Record
-                    </Button>
-                  </div>
-                </div>
-                <div className="grid items-center text-sm justify-items-center w-full">
-                  <div className="font-semibold text-base">
-                    Accepted filetypes:
-                  </div>
-                  <div className="text-base text-center">
-                    {" "}
-                    MP4, WMV, MOV, and AV1
-                  </div>
-                </div>
+                      Upload
+                    </button>
+                  </>
+                )}
               </div>
-            </form>
-          </div>
-        </>
-      ) : (
-        <div className="">
-          <Webcam audio={false} ref={webcamRef} />
-          <div className="flex flex-row justify-evenly p-10">
-            {capturing ? (
-              <>
-                <button onClick={handleStopCaptureClick}>
-                  <Pause />
-                </button>
-                <Button
-                  onClick={handleSaveAndUpload}
-                  disabled={!recordedChunks.length}
-                >
-                  Upload
-                </Button>
-              </>
-            ) : (
-              <>
-                <button onClick={handleStartCaptureClick}>
-                  <Circle className="fill-red-500 stroke-none" />
-                </button>
-                <Button
-                  onClick={handleSaveAndUpload}
-                  disabled={!recordedChunks.length}
-                >
-                  Upload
-                </Button>
-              </>
-            )}
-          </div>
-        </div>
-      )}
+            </div>
+          )}
+        </form>
+      </Card>
     </div>
   );
 };
--- app/obscurus/src/pages/submit/accept.tsx
+++ app/obscurus/src/pages/submit/accept.tsx
@@ -6,28 +6,41 @@ import { CalendarIcon } from "lucide-react";
 import { NextPage } from "next";
 
 export const Accept = () => {
+  const exampleReq = {
+    "Sender" : {
+      "Name":"Walter White",
+      "Email": "walter.white@gmail.com"
+    },
+    "Title": "Cooking",
+    "Description": "Jesse we need to cook! Send me a video of you breaking bad",
+    "Preferences" : {
+      "Video Processing": "Blurred",
+      "Due Date": "January 23, 2024",
+      "Language": "English"
+    }
+  }
   return (
     <div className="p-3">
       <div className="text-2xl font-bold text-center py-5">
         Here is the video request from{" "}
-        <div className=" text-blue-400">Daniel Woods</div>
+        <div className=" text-blue-400">{exampleReq.Sender.Name}</div>
       </div>
       <Card
         id="previewCard"
-        className="drop-shadow-md border-2 border-accent bg-background"
+        className="drop-shadow-md border-2 border-accent bg-card"
       >
         <CardContent className="grid h-full">
           <div id="prevTitle" className="py-6 grid grid-row-3">
             <CardTitle className="break-all text-2xl">
-              Spanish Lesson #2
+              {exampleReq.Title}
             </CardTitle>
           </div>
 
           <div id="prevClient">
             <Label className="font-bold">From</Label>
             <ul>
-              <div className="break-all text-primary indent-2 pt-2 pb-2 font-bold">
-                daniel.woods@gmail.com
+              <div className=" text-primary pt-2 pb-2">
+                {exampleReq.Sender.Email}
               </div>
             </ul>
           </div>
@@ -39,26 +52,26 @@ export const Accept = () => {
               <Label className="font-bold">Request Description</Label>
               <Textarea
                 className="bg-accent resize-none"
-                value={""}
+                value={exampleReq.Description}
                 readOnly
                 rows={9}
               />
             </div>
-            <div className="grid grid-row-4 font-bold">
+            <div className="grid grid-row-4 ">
               <div id="prevBlurred">
-                <Label className="">Video Processing</Label>
-                <div className="w-full ">Blurred</div>
+                <Label className="font-bold">Video Processing</Label>
+                <div className="w-full ">{exampleReq.Preferences["Video Processing"]}</div>
               </div>
               <div id="prevDate" className="pb-1.5 ">
-                <Label>Due Date</Label>
+                <Label className="font-bold">Due Date</Label>
                 <div className="flex items-center">
                   <CalendarIcon className="mr-2" height={20} width={20} />
-                  <div className="text-base">December 3, 2023</div>
+                  <div className="text-base">{exampleReq.Preferences["Due Date"]}</div>
                 </div>
               </div>
               <div id="prevLanguage" className="pb-1.5">
-                <Label>Video Language</Label>
-                <div className="w-full">Language</div>
+                <Label className="font-bold">Video Language</Label>
+                <div className="w-full">{exampleReq.Preferences.Language}</div>
               </div>
               <div id="prevTerms" className="flex items-center space-x-10">
                 <Label className="text-sm font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-none">
