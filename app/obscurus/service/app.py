8b0e98052038b7ed5f5ca4275480885565c2fd02
jansdhillon <jandhilL@student.ubc.ca>
2024-01-23 17:53:03 -0800

    init

--- app/obscurus/service/app.py
+++ app/obscurus/service/app.py
@@ -1,67 +1,71 @@
-import json
-import logging
-import os
-
 import boto3
-import botocore
-import cv2
+import os
+import time
+from video_processor import anonymize_face_pixelate, apply_faces_to_video, integrate_audio
 
-from video_processor import apply_faces_to_video, integrate_audio
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-reko = boto3.client('rekognition')
+# Configure AWS clients
+rekognition = boto3.client('rekognition')
 s3 = boto3.client('s3')
 
+# Environment Variables
+input_bucket = os.environ['INPUT_BUCKET']
+input_name = os.environ['OBJECT_KEY']
 output_bucket = os.environ['OUTPUT_BUCKET']
+output_name = os.environ['OUTPUT_NAME']
 
-def lambda_function(event, context):
-    # download file locally to /tmp retrieve metadata
-    try:
-        response = event['response']
-        # get metadata of file uploaded to Amazon S3
-        bucket = event['s3_object_bucket']
-        key = event['s3_object_key']
-        filename = key.split('/')[-1]
-        local_filename = '/tmp/{}'.format(filename)
-        local_filename_output = '/tmp/anonymized-{}'.format(filename)
-    except KeyError:
-        error_message = 'Lambda invoked without S3 event data. Event needs to reference a S3 bucket and object key.'
-        logger.log(logging.ERROR, error_message)
-        # add_failed(bucket, error_message, failed_records, key)
+def start_face_detection():
+    response = rekognition.start_face_detection(
+        Video={'S3Object': {'Bucket': input_bucket, 'Name': input_name}}
+    )
+    return response['JobId']
 
-    try:
-        s3.download_file(bucket, key, local_filename)
-    except botocore.exceptions.ClientError:
-        error_message = 'Lambda role does not have permission to call GetObject for the input S3 bucket, or object does not exist.'
-        logger.log(logging.ERROR, error_message)
-        # add_failed(bucket, error_message, failed_records, key)
-        # continue
+def check_job_status(job_id):
+    while True:
+        response = rekognition.get_face_detection(JobId=job_id)
+        status = response['JobStatus']
+        if status in ['SUCCEEDED', 'FAILED']:
+            return response
+        time.sleep(5)
 
-        # get timestamps
-    try:
-        timestamps = event['timestamps']
-        apply_faces_to_video(timestamps, local_filename, local_filename_output, response["VideoMetadata"])
-    except Exception as e:
-        print(e)
-        # continue
+def get_timestamps_and_faces(job_id, reko_client=None):
+    final_timestamps = {}
+    next_token = None
+    first_round = True
+    while next_token or first_round:
+        print('.', end='')
+        first_round = False
+        # Query Rekognition for face detection results
+        response = reko_client.get_face_detection(JobId=job_id, MaxResults=100, NextToken=next_token if next_token else "")
+        # Iterate over each face detected and organize by timestamp
+        for face in response['Faces']:
+            f = face["Face"]["BoundingBox"]
+            t = str(face["Timestamp"])
+            if t not in final_timestamps:
+                final_timestamps[t] = []
+            final_timestamps[t].append(f)
+        # Get next token for pagination
+        next_token = response.get('NextToken', None)
+    print('Complete')
+    return final_timestamps, response
 
-    try:
-        integrate_audio(local_filename, local_filename_output)
-    except Exception as e:
-        print(e)
 
-    # uploaded modified video to Amazon S3 bucket
-    try:
-        s3.upload_file(local_filename_output, output_bucket, key)
-    except boto3.exceptions.S3UploadFailedError:
-        error_message = 'Lambda role does not have permission to call PutObject for the output S3 bucket.'
-        # add_failed(bucket, error_message, failed_records, key)
-        # continue
+def process_video(timestamps, response):
+    local_filename = '/tmp/{}'.format(input_name.split('/')[-1])
+    local_filename_output = '/tmp/anonymized-{}'.format(local_filename)
+    s3.download_file(input_bucket, input_name, local_filename)
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Faces in video blurred')
-    }
+    apply_faces_to_video(timestamps, local_filename, local_filename_output, response["VideoMetadata"])
+    integrate_audio(local_filename, local_filename_output)
 
+    s3.upload_file(local_filename_output, output_bucket, output_name)
+
+def main():
+    job_id = start_face_detection()
+    job_response, _ = check_job_status(job_id)
+    timestamps = get_timestamps_and_faces(job_response)
+    process_video(timestamps, job_response)
+
+    print('Video processing completed')
+
+if __name__ == "__main__":
+    main()
