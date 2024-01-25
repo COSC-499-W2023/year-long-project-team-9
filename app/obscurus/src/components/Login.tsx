0ff7f486d9be70c5ad7f0b5eaff14ccd8f45e586
Existential-Critic <soren.is@hotmail.ca>
2024-01-25 14:23:02 -0800

    Added state variables to change sign in box to sign up box
    See title.

--- app/obscurus/src/components/Login.tsx
+++ app/obscurus/src/components/Login.tsx
@@ -37,6 +37,7 @@ const Login = () => {
   const SignOut = () => {
     setSignedIn(false);
   };
+  const [dialogState, setDialogState] = useState(1);
   return (
     <div>
       {signedIn ? (
@@ -66,50 +67,55 @@ const Login = () => {
                 <span className="font-bold text-base">Sign In</span>
               </Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[300px]">
-              <DialogHeader>
-                <DialogTitle className="text-center">Sign In</DialogTitle>
-              </DialogHeader>
-              <div className="flex flex-col w-full items-center">
-                <div id="googleLogin" className="pb-2">
-                  <Button
-                    className="h-40px bg-white dark:bg-[#131314] border-[#747775] dark:border-[#8E918F] border-[1px] text-[#1F1F1F] dark:text-[#E3E3E3] text-sm font-roboto font-medium px-3 py-2.5 hover:bg-transparent"
-                    onClick={SignIn}
-                  >
-                    <Image
-                      src="/Google_Logo.svg"
-                      alt="google"
-                      width={20}
-                      height={20}
-                    />
-                    <span className="pl-2.5">Log in with Google</span>
-                  </Button>
-                </div>
-                <div id="loginSeparator" className="relative pb-2">
-                  <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-muted-foreground">
-                      Or continue with
-                    </span>
+            {dialogState === 1 && (
+              <DialogContent className="sm:max-w-[300px]">
+                <DialogHeader>
+                  <DialogTitle className="text-center">Sign In</DialogTitle>
+                </DialogHeader>
+                <div className="flex flex-col w-full items-center">
+                  <div id="googleLogin" className="pb-2">
+                    <Button
+                      className="h-40px bg-white dark:bg-[#131314] border-[#747775] dark:border-[#8E918F] border-[1px] text-[#1F1F1F] dark:text-[#E3E3E3] text-sm font-roboto font-medium px-3 py-2.5 hover:bg-transparent"
+                      onClick={SignIn}
+                    >
+                      <Image
+                        src="/Google_Logo.svg"
+                        alt="google"
+                        width={20}
+                        height={20}
+                      />
+                      <span className="pl-2.5">Log in with Google</span>
+                    </Button>
+                  </div>
+                  <div id="loginSeparator" className="relative pb-2">
+                    <div className="relative flex justify-center text-xs uppercase">
+                      <span className="bg-background px-2 text-muted-foreground">
+                        Or continue with
+                      </span>
+                    </div>
+                  </div>
+                  <div id="emailLogin" className="grid gap-1.5">
+                    <Input
+                      placeholder="name@example.ca"
+                      className="pb-2"
+                    ></Input>
+                    <Input placeholder="Password"></Input>
+                    <Button className="w-full">
+                      <span>Log in with Email</span>
+                    </Button>
+                    <Button className="bg-background text-primary text-xs justify-self-center hover:bg-transparent shadow-none h-3/4">
+                      Forgot your password?
+                    </Button>
+                  </div>
+                  <hr className="w-3/4 bg-gray-500" />
+                  <div id="signUpConnector" className="w-3/4 text-center">
+                    <Button className="bg-background text-primary text-xs justify-self-center hover:bg-transparent shadow-none h-3/4">
+                      Create New Account
+                    </Button>
                   </div>
                 </div>
-                <div id="emailLogin" className="grid gap-1.5 pb-2">
-                  <Input placeholder="name@example.ca" className="pb-2"></Input>
-                  <Input placeholder="Password"></Input>
-                  <Button className="w-full">
-                    <span>Log in with Email</span>
-                  </Button>
-                  <span className="bg-background px-2 text-muted-foreground text-xs justify-self-center">
-                    Forgot your password?
-                  </span>
-                </div>
-                <hr className="w-3/4 bg-gray-500" />
-                <div id="signUpConnector" className="w-3/4 text-center pt-1">
-                  <Button className="bg-background text-primary text-xs justify-self-center hover:bg-transparent shadow-none h-3/4">
-                    Create New Account
-                  </Button>
-                </div>
-              </div>
-            </DialogContent>
+              </DialogContent>
+            )}
           </Dialog>
         </div>
       )}
