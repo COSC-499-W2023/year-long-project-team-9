2791713b89826e7977a09cc0b6eecbadda767f15
Existential-Critic <soren.is@hotmail.ca>
2024-01-25 14:16:22 -0800

    Created framework for login page
    See title.

--- app/obscurus/src/components/Login.tsx
+++ app/obscurus/src/components/Login.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Dialog,
@@ -28,7 +29,6 @@ import {
   /*FUNCTIONS*/
 }
 const Login = () => {
-  const { theme } = useTheme();
   const [signedIn, setSignedIn] = useState(false);
   const [showSignInDialog, setShowSignInDialog] = useState(false);
   const SignIn = () => {
@@ -66,14 +66,14 @@ const Login = () => {
                 <span className="font-bold text-base">Sign In</span>
               </Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="sm:max-w-[300px]">
               <DialogHeader>
                 <DialogTitle className="text-center">Sign In</DialogTitle>
               </DialogHeader>
-              <div className="flex flex-col items-center gap-4">
-                <div id="googleButton">
+              <div className="flex flex-col w-full items-center">
+                <div id="googleLogin" className="pb-2">
                   <Button
-                    className="h-40px min-w-min bg-white dark:bg-[#131314] border-[#747775] dark:border-[#8E918F] border-[1px] text-[#1F1F1F] dark:text-[#E3E3E3] text-sm font-roboto font-medium px-3 py-2.5 hover:bg-transparent"
+                    className="h-40px bg-white dark:bg-[#131314] border-[#747775] dark:border-[#8E918F] border-[1px] text-[#1F1F1F] dark:text-[#E3E3E3] text-sm font-roboto font-medium px-3 py-2.5 hover:bg-transparent"
                     onClick={SignIn}
                   >
                     <Image
@@ -85,15 +85,28 @@ const Login = () => {
                     <span className="pl-2.5">Log in with Google</span>
                   </Button>
                 </div>
-                <div className="relative">
-                  <div>
-                    <span className="w-full border-t-2" />
-                  </div>
+                <div id="loginSeparator" className="relative pb-2">
                   <div className="relative flex justify-center text-xs uppercase">
                     <span className="bg-background px-2 text-muted-foreground">
                       Or continue with
                     </span>
                   </div>
+                </div>
+                <div id="emailLogin" className="grid gap-1.5 pb-2">
+                  <Input placeholder="name@example.ca" className="pb-2"></Input>
+                  <Input placeholder="Password"></Input>
+                  <Button className="w-full">
+                    <span>Log in with Email</span>
+                  </Button>
+                  <span className="bg-background px-2 text-muted-foreground text-xs justify-self-center">
+                    Forgot your password?
+                  </span>
+                </div>
+                <hr className="w-3/4 bg-gray-500" />
+                <div id="signUpConnector" className="w-3/4 text-center pt-1">
+                  <Button className="bg-background text-primary text-xs justify-self-center hover:bg-transparent shadow-none h-3/4">
+                    Create New Account
+                  </Button>
                 </div>
               </div>
             </DialogContent>
