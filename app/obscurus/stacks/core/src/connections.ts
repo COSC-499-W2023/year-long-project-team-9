9a9a7247d33a6c3f27800310b44103c6a3ff5d52
Existential-Critic <soren.is@hotmail.ca>
2024-02-08 15:06:40 -0800

    Added code for sendMessage
    See title.

--- app/obscurus/stacks/core/src/connections.ts
+++ app/obscurus/stacks/core/src/connections.ts
@@ -3,6 +3,10 @@ export * as Connections from "./connections";
 import { SQL } from "./sql";
 import { Connections } from "./sql.generated";
 
+export function list() {
+  return SQL.DB.selectFrom("connections").selectAll().execute();
+}
+
 export function addConnection(newConnection: Connections) {
   return SQL.DB.insertInto("connections")
     .values({
--- app/obscurus/stacks/lambdas/chat/sendMessage.ts
+++ app/obscurus/stacks/lambdas/chat/sendMessage.ts
@@ -1,5 +1,50 @@
+import { ApiGatewayManagementApi } from "aws-sdk";
 import { APIGatewayProxyHandler } from "aws-lambda";
+import { Connections } from "../../core/src/connections";
+import { Connections as ConnectionsType } from "../../core/src/sql.generated";
 
 export const main: APIGatewayProxyHandler = async (event) => {
-  return { statusCode: 200, body: "Message sent" };
+  if (event.body != null) {
+    // Save applicable data
+    const messageData = JSON.parse(event.body).data;
+    const { stage, domainName } = event.requestContext;
+
+    // Get all connections
+    const connectionsList = await Connections.list();
+    console.log(connectionsList);
+
+    // Get API Gateway
+    const apiG = new ApiGatewayManagementApi({
+      endpoint: `${domainName}/${stage}`,
+    });
+
+    // Helper method
+    const postToConnection = async function ({ connection_id }) {
+      try {
+        await apiG
+          .postToConnection({ ConnectionId: connection_id, Data: messageData })
+          .promise();
+        console.log(connection_id);
+      } catch (e) {
+        if (e.statusCode === 410) {
+          // Remove stale connections
+          const staleConnection: ConnectionsType = {
+            connection_id: connection_id,
+          };
+          const delConnection = await Connections.removeConnection(
+            staleConnection
+          );
+        }
+      }
+    };
+
+    // Iterate through all connections
+    await Promise.all(connectionsList.map(postToConnection));
+
+    // Return status code
+    return { statusCode: 200, body: "Message sent" };
+  }
+  console.log("Didn't work");
+  // Return status code
+  return { statusCode: 500, body: "Error, undefined" };
 };
