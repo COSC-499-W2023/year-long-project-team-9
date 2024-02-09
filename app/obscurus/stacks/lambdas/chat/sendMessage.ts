f3bc64f662d10df4dd6eccb96d626abe14a7e632
Existential-Critic <soren.is@hotmail.ca>
2024-02-09 10:23:47 -0800

    Removed console.logs from sendMessage.ts
    See title.

--- app/obscurus/stacks/lambdas/chat/sendMessage.ts
+++ app/obscurus/stacks/lambdas/chat/sendMessage.ts
@@ -4,14 +4,13 @@ import { Connections } from "../../core/src/connections";
 import { Connections as ConnectionsType } from "../../core/src/sql.generated";
 
 export const main: APIGatewayProxyHandler = async (event) => {
-  if (event.body != undefined) {
+  if (event.body != null) {
     // Save applicable data
     const messageData = JSON.parse(event.body).data;
     const { stage, domainName } = event.requestContext;
 
     // Get all connections
     const connectionsList = await Connections.list();
-    console.log(connectionsList);
 
     // Get API Gateway
     const apiG = new ApiGatewayManagementApi({
@@ -24,7 +23,6 @@ export const main: APIGatewayProxyHandler = async (event) => {
         await apiG
           .postToConnection({ ConnectionId: connection_id, Data: messageData })
           .promise();
-        console.log(connection_id);
       } catch (e) {
         if (e.statusCode === 410) {
           // Remove stale connections
@@ -44,7 +42,6 @@ export const main: APIGatewayProxyHandler = async (event) => {
     // Return status code
     return { statusCode: 200, body: "Message sent" };
   }
-  console.log("Didn't work");
   // Return status code
   return { statusCode: 500, body: "Error, undefined" };
 };
