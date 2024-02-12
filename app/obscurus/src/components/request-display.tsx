fd1e61ba2ec4e47f6ce19f382212d32745f050c1
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-02-12 10:02:11 -0800

    adding two component

--- app/obscurus/src/components/request-display.tsx
+++ app/obscurus/src/components/request-display.tsx
@@ -1,97 +1,102 @@
-"use client";
-import {
-  CalendarIcon,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { Requests } from "stacks/core/src/sql.generated";
-import { useRouter } from "next/navigation";
-import { Card, CardContent, CardTitle } from "./ui/card";
+import { Archive, FileVideo2, Trash } from "lucide-react";
+import { Button } from "./ui/button";
 import { useState } from "react";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
 
-export default function RequestDisplay({ request }: { request: Requests }) {
-  const today = new Date();
-  console.log(request);
-  const router = useRouter();
-  const [isCollapsed, setIsCollapsed] = useState(false);
+type RequestDisplayProps = {
+  title: string;
+  processing: string;
+  emails: string[];
+  description: string;
+  date: string;
+  due_date: string;
+  id: string;
+  sendToArchive: any;
+  sendToTrash: any;
+};
+export function RequestDisplay({
+  title,
+  processing,
+  emails,
+  description,
+  date,
+  due_date,
+  id,
+  sendToArchive,
+  sendToTrash,
+}: RequestDisplayProps) {
+  const [isTableShowing, setIsTableShowing] = useState(false);
+  const emailList = emails.join(", ");
+  function showTable(isTableShowing: boolean) {
+    setIsTableShowing(!isTableShowing);
+  }
   return (
-    <div id="previewCard" className=" h-max bg-card">
-      <CardContent className="grid">
-        <div className="grid">
-          <div id="prevTitle" className="pt-6 grid grid-row-3">
-            <CardTitle className="break-all text-2xl" id="requestTitle">
-              {request.request_title}
-            </CardTitle>
+    <>
+      <div className="flex h-full flex-col">
+        <div className="flex items-center p-2">
+          <div className="flex items-center gap-2">
+            <Button variant="ghost" size="icon" onClick={() => sendToTrash(id)}>
+              <Trash className="h-4 w-4" />
+              <span className="sr-only">Trash</span>
+            </Button>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => sendToArchive(id)}
+            >
+              <Archive className="h-4 w-4" />
+              <span className="sr-only">Archive</span>
+            </Button>
           </div>
-
-          <div id="prevClient">
-            <Label className="font-bold">From</Label>
-            <div className="break-all text-primary pb-2 text-sm font-medium ">
-              {request.requester_sub}
-            </div>
-          </div>
-
-          <div
-            id="prevDescAndData"
-            className="grid grid-cols-2 left-justify gap-5 pt-2"
-          >
-            <div id="prevDesc">
-              <Label className="font-bold py-5">Request Description</Label>
-
-              <Textarea
-                className="bg-accent resize-none"
-                value={request.description}
-                readOnly
-                rows={14}
-              />
-            </div>
-            <div className="grid grid-row-4">
-              <div id="prevDate" className=" ">
-                <Label className="font-bold">Due Date</Label>
-                <div className="flex items-center">
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  <div className="text-base">{request.due_date.toString()}</div>
-                </div>
-              </div>
-              <div className="grid grid-row-4">
-                <div id="prevBlurred" className="pb-1.5">
-                  {" "}
-                  <Label className="font-bold">Video Processing</Label>
-                  <div className="w-full font-base">Blurred</div>
-                </div>
-                <div id="prevLanguage" className="pb-1.5">
-                  <Label className="font-bold">Video Language</Label>
-                  <div className="w-full">English</div>
-                </div>
-                <div id="prevTerms" className="flex items-center space-x-10">
-                  <Label className="text-sm font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-none">
-                    See the{" "}
-                    <a
-                      href=""
-                      target="_blank"
-                      className="text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      terms and conditions
-                    </a>{" "}
-                    here
-                  </Label>
-                </div>
-              </div>
-            </div>
+          <div className="ml-auto flex items-center gap-2">
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => showTable(isTableShowing)}
+            >
+              <FileVideo2 className="h-4 w-4" />
+              <span className="sr-only">Video</span>
+            </Button>
           </div>
         </div>
-      </CardContent>
-      <div className="flex justify-end p-10">
-        <Button
-          id="submitButton"
-          type="submit"
-          className=" justify-self-start font-bold p-5 "
-          onClick={() => router.push("/submit")}
-        >
-          Upload Video
-        </Button>
+        <div className="flex flex-1 flex-col">
+          <div className="flex items-start p-4">
+            <div className="flex items-start gap-4 text-sm">
+              <div className="grid gap-1">
+                <div className="font-semibold">{title}</div>
+                <div className="line-clamp-1 text-xs">
+                  Processing: {processing}
+                </div>
+                <div className="line-clamp-1 text-xs">Due: {due_date}</div>
+                <HoverCard>
+                  <HoverCardTrigger className="line-clamp-1 text-xs">
+                    To: {emailList}
+                  </HoverCardTrigger>
+                  <HoverCardContent>
+                    To:
+                    <ul>
+                      {emails.map((item, index) => (
+                        <li className="pl-1">• {item}</li>
+                      ))}
+                    </ul>
+                  </HoverCardContent>
+                </HoverCard>
+              </div>
+            </div>
+            <div className="ml-auto text-xs text-muted-foreground">{date}</div>
+          </div>
+          <div
+            data-orientation="horizontal"
+            role="none"
+            className="shrink-0 bg-border h-[1px] w-full"
+          ></div>
+          {isTableShowing === false ? (
+            <div className="flex-1 whitespace-pre-wrap p-4 text-sm">
+              {description}
+            </div>
+          ) : null}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
