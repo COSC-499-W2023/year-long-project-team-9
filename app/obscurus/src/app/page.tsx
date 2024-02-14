edee22a7466194341b34356bf6d9efa8f0e7c217
Existential-Critic <soren.is@hotmail.ca>
2024-02-14 14:31:51 -0800

    Replaced Submit call due to error in Submit
    See title.

--- app/obscurus/src/app/page.tsx
+++ app/obscurus/src/app/page.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from "react";
 import Home from "./Home/page";
-import Submit  from "./submit/page";
+import Submit from "./submit/page";
 import Wrapper from "./wrapper";
+import MyRequests from "./MyRequests/page";
 import { ResizableHandle, ResizablePanel } from "@/components/ui/resizable";
 
 function Page() {
   return (
- 
-      <Submit/>
+    <MyRequests />
+    // <Submit/>
   );
 }
 
