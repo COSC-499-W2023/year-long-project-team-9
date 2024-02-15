274ec143dfffa68875f7a07106bfd33ef95fc8ad
Existential-Critic <soren.is@hotmail.ca>
2024-02-15 15:04:57 -0800

    Added extra dummy code to Chat
    See title.
    
    Co-Authored-By: asiv23 <77401969+asiv23@users.noreply.github.com>

--- app/obscurus/src/app/Chat/page.tsx
+++ app/obscurus/src/app/Chat/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 // IMPORTS
+import { Suspense } from "react";
+import { ResizableHandle, ResizablePanel } from "@/components/ui/resizable";
 
 // CONSTRUCTS
 
 // FUNCTIONS
 const Chat = () => {
-  return <></>;
+  return (
+    <>
+      <ResizablePanel defaultSize={50}>
+        <div className=" h-full flex-1 flex-col space-y-8 p-8 md:flex">
+          <div className="flex items-center justify-between space-y-2">
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">Chat</h2>
+            </div>
+          </div>
+          <Suspense>
+            {/* <DataTable data={submissions} columns={columns} /> */}
+          </Suspense>
+        </div>
+      </ResizablePanel>
+      <ResizablePanel defaultSize={50}>
+        <div></div>
+      </ResizablePanel>
+    </>
+  );
 };
 
 // EXPORT
