a5ad86b0d063647dc05e84dca3bfff311ebe3ef8
Existential-Critic <soren.is@hotmail.ca>
2024-02-18 19:22:21 -0800

    Added unfinished function to create a map of rooms and messages
    See title.

--- app/obscurus/src/app/Chat/page.tsx
+++ app/obscurus/src/app/Chat/page.tsx
@@ -15,9 +15,34 @@ async function getRooms(email: string) {
     return res.json();
   }
 }
+function createRoomsMap(roomMessageData: {}) {
+  const roomsMap = new Map();
+  const roomsLength = Object.keys(roomMessageData.rooms).length;
+  const messagesLength = Object.keys(roomMessageData.messages).length;
+  for (let i = 0; i < roomsLength; i++) {
+    const roomId = roomMessageData.rooms[i].roomId;
+    const roomData = roomMessageData.rooms[i];
+    const roomMessages = {};
+    for (let j = 0; j < messagesLength; j++) {
+      const messageRoomId = roomMessageData.messages[j].roomId;
+      if (messageRoomId === roomId) {
+        roomMessages[roomMessageData.messages[j].messageId] =
+          roomMessageData.messages[j];
+      }
+    }
+    const mapData = {};
+  }
+  return roomsMap;
+}
 export default async function ChatPage() {
+  // Getting room and message data
   const roomMessageData = await getRooms(userEmail);
-  console.log(roomMessageData);
+  // console.log(roomMessageData);
+  // Adding rooms and messages to a map object
+  const roomsMap = createRoomsMap(roomMessageData);
+  console.log(roomsMap);
+
+  // Saving layout from cookies
   const layout = cookies().get("react-resizable-panels:layout");
   const collapsed = cookies().get("react-resizable-panels:collapsed");
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
