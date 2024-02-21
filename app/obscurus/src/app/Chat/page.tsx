a2565d2a9527393d4498b1674c12eab0b0ecf808
Existential-Critic <soren.is@hotmail.ca>
2024-02-21 12:21:14 -0800

    Updated getRoomsViaEmail to only get rooms and turn into array
    See title.

--- app/obscurus/src/app/Chat/page.tsx
+++ app/obscurus/src/app/Chat/page.tsx
@@ -4,7 +4,7 @@ import { Api } from "sst/node/api";
 import { cookies } from "next/headers";
 import { Suspense } from "react";
 import { ResizableHandle, ResizablePanel } from "@/components/ui/resizable";
-import { Requests, Submissions } from "stacks/core/src/sql.generated";
+import { Rooms, Messages } from "stacks/core/src/sql.generated";
 import { getSubmissions } from "../functions/getSubmissions";
 import { getRequests } from "../functions/getRequests";
 import SubmitDisplay from "../Submit/components/submit-display";
@@ -17,15 +17,21 @@ const userEmail = "imightbejan@gmail.com";
 
 // FUNCTIONS
 async function Chat() {
-  // Getting room and message data
-  const roomMessageData = await getRoomsViaEmail(userEmail);
-  // console.log(roomMessageData);
-
   // Saving layout from cookies
   const layout = cookies().get("react-resizable-panels:layout");
   const collapsed = cookies().get("react-resizable-panels:collapsed");
+  // console.log("Layout", layout);
+  // console.log("Collapsed", collapsed?.value);
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
-  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+  const defaultCollapsed =
+    collapsed && collapsed.value !== "undefined"
+      ? JSON.parse(collapsed.value)
+      : undefined;
+
+  // Getting room and message data
+  const rooms: Rooms[] = await getRoomsViaEmail(userEmail);
+  console.log(rooms);
+
   return (
     <Wrapper
       defaultLayout={defaultLayout}
