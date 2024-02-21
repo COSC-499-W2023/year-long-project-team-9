8c6004b530f73dfe895bc25759f36115307f45f2
jansdhillon <jandhilL@student.ubc.ca>
2024-02-20 21:17:01 -0800

    fix skeleton color

--- app/obscurus/src/app/loading.tsx
+++ app/obscurus/src/app/loading.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from "@/components/ui/skeleton"
 const Loading = () => {
     return(
         <div className=" w-full h-screen flex space-x-5 flex-row overflow-hidden px-5 pb-28 pt-5">
-            <Skeleton className="w-[15%] " />
-            <Skeleton className="w-[35%]" />
-            <Skeleton className="w-[50%]" />
+            <Skeleton className="w-[15%] bg-accent " />
+            <Skeleton className="w-[35%]  bg-accent" />
+            <Skeleton className="w-[50%]  bg-accent" />
         </div>
     )
 }
