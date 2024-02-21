f78acd0cffb8d1fb8827694a233059f37a18caf5
Existential-Critic <soren.is@hotmail.ca>
2024-02-21 12:00:02 -0800

    Updated Chat/page.tsx with Wrapper
    See title.

--- app/obscurus/src/app/Chat/page.tsx
+++ app/obscurus/src/app/Chat/page.tsx
@@ -54,32 +54,12 @@ export default async function ChatPage() {
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
   return (
-    <>
-      <ResizablePanel defaultSize={50}>
-        <div className=" h-full flex-1 flex-col space-y-8 p-8 md:flex">
-          <div className="flex items-center justify-between space-y-2">
-            <div>
-              <h2 className="text-2xl font-bold tracking-tight">Chat</h2>
-            </div>
-          </div>
-          <Suspense>
-            {/* <DataTable data={submissions} columns={columns} /> */}
-          </Suspense>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={50}>
-        <div className=" h-full flex-1 flex-col space-y-8 p-8 md:flex">
-          <div className="flex items-center justify-between space-y-2">
-            <div>
-              <h2 className="text-2xl font-bold tracking-tight">Messages</h2>
-            </div>
-          </div>
-          <Suspense>
-            {/* <DataTable data={submissions} columns={columns} /> */}
-          </Suspense>
-        </div>
-      </ResizablePanel>
-    </>
+    <Wrapper
+      defaultLayout={defaultLayout}
+      defaultCollapsed={defaultCollapsed}
+      navCollapsedSize={4}
+      firstPanel={""}
+      secondPanel={""}
+    />
   );
 }
