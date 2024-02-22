974b60c43a26ea5df03eaf12477611838be8bf1b
Existential-Critic <soren.is@hotmail.ca>
2024-02-21 17:23:25 -0800

    Added other user's avatar in chatroom
    See title.
    
    Co-Authored-By: asiv23 <77401969+asiv23@users.noreply.github.com>

--- app/obscurus/src/app/Chat/components/chat-display.tsx
+++ app/obscurus/src/app/Chat/components/chat-display.tsx
@@ -26,32 +26,33 @@ export default function ChatDisplay({ rooms, messages }: ChatsDisplayProps) {
   console.log("RoomId", roomId);
 
   const selected = rooms.find((item) => item.roomId === roomId);
+  var otherUserName = "";
+  if (selected?.participant1Email === userEmail) {
+    otherUserName =
+      selected.participant2RoomGivenName +
+      " " +
+      selected.participant2RoomFamilyName;
+  } else if (selected?.participant2Email === userEmail) {
+    otherUserName =
+      selected.participant1RoomGivenName +
+      " " +
+      selected.participant1RoomFamilyName;
+  }
   console.log("Selected room to display", selected);
 
   return selected ? (
     <div className="flex h-full flex-col min-h-full">
-      <div className="flex items-center justify-center p-2">
-        <div className="flex items-center gap-2"></div>
-        <Avatar className="">
-          <AvatarImage alt={userEmail} />
+      <div className="flex flex-col items-center justify-center p-2">
+        <Avatar>
+          <AvatarImage alt={otherUserName} />
           <AvatarFallback>
-            {userEmail
+            {otherUserName
               .split(" ")
               .map((chunk) => chunk[0])
               .join("")}
           </AvatarFallback>
         </Avatar>
-        <div className="font-semibold">
-          {selected.participant1Email === userEmail
-            ? selected.participant2RoomGivenName +
-                " " +
-                selected.participant2RoomFamilyName ||
-              selected.participant2Email
-            : selected.participant1RoomGivenName +
-                " " +
-                selected.participant1RoomFamilyName ||
-              selected.participant1Email}
-        </div>
+        <div className="font-semibold">{otherUserName}</div>
       </div>
       <Separator />
       <Suspense
