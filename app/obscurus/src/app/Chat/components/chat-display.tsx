d632499075844223da025a00a9411cd8fc3b6776
Existential-Critic <soren.is@hotmail.ca>
2024-02-21 17:06:59 -0800

    Updated imports of chat-display.tsx
    See title.
    
    Co-Authored-By: asiv23 <77401969+asiv23@users.noreply.github.com>

--- app/obscurus/src/app/Chat/components/chat-display.tsx
+++ app/obscurus/src/app/Chat/components/chat-display.tsx
@@ -1,26 +1,22 @@
 "use client";
+// IMPORTS
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { Rooms, Messages } from "stacks/core/src/sql.generated";
 import { useQueryState } from "nuqs";
-import { Suspense, useState } from "react";
-import { Archive, Trash2, ArrowLeft } from "lucide-react";
+import { Suspense } from "react";
 import { formatDistanceToNow, formatDistance, format } from "date-fns";
-import Upload from "../../Submit/components/upload";
 
-export default function ChatDisplay({
-  rooms,
-  messages,
-}: {
+// CONSTRUCTS
+const userEmail = "imightbejan@gmail.com";
+interface ChatsDisplayProps {
   rooms: Rooms[];
   messages: Messages[];
-}) {
+}
+
+// FUNCTIONS
+export default function ChatDisplay({ rooms, messages }: ChatsDisplayProps) {
   const [roomId, setRoomId] = useQueryState("roomId");
   const [submissionId, setSubmissionId] = useQueryState("submissionId");
   const [upload, setUpload] = useQueryState("upload");
@@ -40,6 +36,15 @@ export default function ChatDisplay({
       <div className="flex items-center p-2">
         <div className="flex items-center gap-2"></div>
       </div>
+      <Avatar>
+        <AvatarImage alt={userEmail} />
+        <AvatarFallback>
+          {userEmail
+            .split(" ")
+            .map((chunk) => chunk[0])
+            .join("")}
+        </AvatarFallback>
+      </Avatar>
       <Separator />
       <Suspense
         fallback={
@@ -47,9 +52,7 @@ export default function ChatDisplay({
             Loading...
           </div>
         }
-      >
-        <Upload />
-      </Suspense>
+      ></Suspense>
     </div>
   ) : (
     <div>Failed to load data....</div>
