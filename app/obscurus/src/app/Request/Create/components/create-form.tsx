b7edb60404e256b8252759a38392b410097da895
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-02-22 20:39:38 -0800

    adding form validation

--- app/obscurus/src/app/Request/Create/components/create-form.tsx
+++ app/obscurus/src/app/Request/Create/components/create-form.tsx
@@ -6,13 +6,28 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { addDays, endOfDay, format } from "date-fns";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
 import {
   Select,
   SelectContent,
@@ -20,120 +35,198 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Calendar } from "@/components/ui/calendar";
-import { addDays, format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
-import { cn } from "@/lib/utils";
+
+// Create Request from Schema
+const formSchema = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(1, {
+      message: "Title must be at least 1 character.",
+    })
+    .max(100, {
+      message: "Title must no more than 100 characters.",
+    }),
+  isblurred: z.boolean().default(true),
+  requestDueDate: z.date().min(endOfDay(new Date()), {
+    message: "Due must be no earlier than today.",
+  }),
+  requestDescription: z
+    .string()
+    .trim()
+    .min(1, {
+      message: "Request Description must be at least 1 character.",
+    })
+    .max(2000, {
+      message: "Request Description must no more than 2000 characters.",
+    }),
+});
 
 export default function CreateForm() {
-  const [date, setDate] = React.useState<Date>();
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
     <>
       <CreateHeader />
-      <ScrollArea className="flex flex-row">
-        {/* Request Title */}
-        <div>
-          <Label>Request Title</Label>
-        </div>
-        <div>
-          <Input type="string" placeholder="Title" />
-        </div>
-        {/* Client emails */}
-        <Label>Client</Label>
-        <div>
-          <div>
-            <Input type="email" placeholder="Email" />
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-2">
-          <div>
-            {/* Video Processing */}
-            <div>
-              <Label>Video Processing</Label>
-            </div>
-            <Tabs defaultValue="blurred">
-              <TabsList className="w-full">
-                <TabsTrigger value="normal" className="w-full">
-                  Normal
-                </TabsTrigger>
-                <TabsTrigger value="blurred" className="w-full">
-                  Blurred
-                </TabsTrigger>
-              </TabsList>
-              <TabsContent value="normal">{/* TODO  */}</TabsContent>
-              <TabsContent value="blurred">{/* TODO  */}</TabsContent>
-            </Tabs>
-          </div>
-          {/* Due Date */}
-          <div>
-            <Label>Due Date</Label>
-            <div>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant={"outline"}
-                    className={cn(
-                      "w-full justify-center text-left font-normal",
-                      !date && "text-muted-foreground"
-                    )}
-                  >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent
-                  align="start"
-                  className="flex w-auto flex-col space-y-2 p-2"
-                >
-                  <Select
-                    onValueChange={(value) =>
-                      setDate(addDays(new Date(), parseInt(value)))
-                    }
-                  >
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="0">Today</SelectItem>
-                      <SelectItem value="1">Tomorrow</SelectItem>
-                      <SelectItem value="3">In 3 days</SelectItem>
-                      <SelectItem value="7">In a week</SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <div className="rounded-md border">
-                    <Calendar
-                      mode="single"
-                      selected={date}
-                      onSelect={setDate}
-                    />
-                  </div>
-                </PopoverContent>
-              </Popover>
-            </div>
-          </div>
-        </div>
-        {/* Request Description  */}
-        <Label>Request Description</Label>
-        <div className="h-full">
-          <Textarea
-            placeholder="Description"
-            className="resize-none"
-            rows={18} // TODO: Change h-full
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* Request Title */}
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Request Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" maxLength={101} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </div>
-        <div className="text-right gap-2 py-2">
-          <Button type="button" variant="ghost" className="justify-self-start">
-            Cancel Request
-          </Button>
-          <Button
-            type="submit"
-            variant="default"
-            className="justify-self-start"
-          >
-            Submit Request
-          </Button>
-        </div>
-      </ScrollArea>
+          <div className="grid sm:grid-cols-2 gap-2">
+            <FormField
+              control={form.control}
+              name="requestDueDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Video Processing</FormLabel>
+                  <Tabs defaultValue="blurred" className="w-full">
+                    <TabsList className="w-full">
+                      <TabsTrigger
+                        value="normal"
+                        className="w-full"
+                        onClick={() => form.setValue("isblurred", false)}
+                      >
+                        Normal
+                      </TabsTrigger>
+                      <TabsTrigger
+                        value="blurred"
+                        className="w-full"
+                        onClick={() => form.setValue("isblurred", false)}
+                      >
+                        Blurred
+                      </TabsTrigger>
+                    </TabsList>
+                  </Tabs>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            {/* Due Date */}
+            <FormField
+              control={form.control}
+              name="requestDueDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Due Date</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full pl-3 text-center font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent
+                      align="start"
+                      className="flex w-auto flex-col space-y-2 p-2"
+                    >
+                      <Select
+                        onValueChange={(value) =>
+                          form.setValue(
+                            "requestDueDate",
+                            endOfDay(
+                              new Date(
+                                new Date().setDate(
+                                  new Date().getDate() + Number(value)
+                                )
+                              )
+                            )
+                          )
+                        }
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select" />
+                        </SelectTrigger>
+                        <SelectContent position="popper">
+                          <SelectItem value="0">Today</SelectItem>
+                          <SelectItem value="1">Tomorrow</SelectItem>
+                          <SelectItem value="3">In 3 days</SelectItem>
+                          <SelectItem value="7">In a week</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={(value) =>
+                          value === undefined
+                            ? form.setValue(
+                                "requestDueDate",
+                                new Date("1973-02-02")
+                              )
+                            : form.setValue("requestDueDate", endOfDay(value))
+                        }
+                        disabled={(date) =>
+                          date <
+                          new Date(new Date().setDate(new Date().getDate() - 1))
+                        }
+                        initialFocus
+                        today={new Date()}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          {/* Request Description */}
+          <FormField
+            control={form.control}
+            name="requestDescription"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Request Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Description"
+                    className="resize-none"
+                    {...field}
+                    maxLength={2001}
+                    rows={18} // TODO: Change h-full
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="text-right gap-2 py-2">
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </Form>
     </>
   );
 }
---
+++ app/obscurus/src/app/request/create/components/due-date-picker.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { FormControl } from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { addDays, format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import React from "react";
+
+export default function DueDatePicker(field: any) {
+  return (
+    <>
+      <Popover>
+        <PopoverTrigger asChild>
+          <FormControl>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-full pl-3 text-left font-normal",
+                !field.value && "text-muted-foreground"
+              )}
+            >
+              {field.value ? (
+                format(field.value, "PPP")
+              ) : (
+                <span>Pick a date</span>
+              )}
+              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+            </Button>
+          </FormControl>
+        </PopoverTrigger>
+        <PopoverContent className="w-full p-0" align="start">
+          <Select
+            onValueChange={(value) =>
+              setDate(addDays(new Date(), parseInt(value)))
+            }
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              <SelectItem value="0">Today</SelectItem>
+              <SelectItem value="1">Tomorrow</SelectItem>
+              <SelectItem value="3">In 3 days</SelectItem>
+              <SelectItem value="7">In a week</SelectItem>
+            </SelectContent>
+          </Select>
+          <Calendar
+            className="w-full"
+            mode="single"
+            selected={field.value}
+            onSelect={field.onChange}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+}
