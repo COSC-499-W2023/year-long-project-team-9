38b9662e38a1057e2c869fc65169066a56fb1392
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-02-25 13:39:57 -0800

    basic form set up form shadcn

--- app/obscurus/src/app/request/create/components/create-form.tsx
+++ app/obscurus/src/app/request/create/components/create-form.tsx
@@ -1,15 +1,63 @@
 "use client";
 import CreateHeader from "./create-header";
 import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(100),
+});
 
 interface CreateFormProps {
-  email: string;
+  userEmail: string;
 }
 
-export default function CreateForm({ email }: CreateFormProps) {
+export default function CreateForm({ userEmail }: CreateFormProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
     <div>
       <CreateHeader />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 }
