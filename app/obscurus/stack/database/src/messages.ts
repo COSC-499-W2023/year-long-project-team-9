21d49b35b9174c5e3651cbe6f7faac2068c477cb
Existential-Critic <soren.is@hotmail.ca>
2024-02-27 22:39:15 -0800

    Created lambda function for adding new message to database
    See title.
    
    Co-Authored-By: asiv23 <77401969+asiv23@users.noreply.github.com>

--- app/obscurus/stack/database/src/messages.ts
+++ app/obscurus/stack/database/src/messages.ts
@@ -8,3 +8,16 @@ import { Status } from "./types/status";
 export function list() {
   return SQL.DB.selectFrom("messages").selectAll().execute();
 }
+
+export function insert(message: Messages) {
+  return SQL.DB.insertInto("messages")
+    .values({
+      messageId: message.messageId,
+      roomId: message.roomId,
+      senderEmail: message.senderEmail,
+      creationDate: message.creationDate,
+      messageContent: message.messageContent,
+      isRead: message.isRead,
+    })
+    .execute();
+}
