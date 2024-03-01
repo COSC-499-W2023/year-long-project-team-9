c10e5ab0b5194a805833832da73dba3b7cd5b293
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-02-29 18:42:56 -0800

    preventing client email from being user email

--- app/obscurus/src/app/request/create/components/create-form.tsx
+++ app/obscurus/src/app/request/create/components/create-form.tsx
@@ -22,35 +22,48 @@ import ClientEmail from "./create-form-client-input";
 import CreateFormDueDateInput from "./create-form-due-date-input";
 import CreateCancel from "./create-from-cancel";
 
-const createFormSchema = z.object({
-  title: z.string().trim().min(1).max(100),
-  dueDate: z.date().min(endOfDay(new Date())),
-  description: z.string().trim().min(1).max(2000),
-  clientEmail: z
-    .array(
-      z.object({
-        email: z.string().trim().toLowerCase().min(1).max(320).email(),
-      })
-    )
-    .superRefine((clientEmail, ctx) => {
-      for (let i = 0; i < clientEmail.length; i++) {
-        for (let j = 0; j < clientEmail.length; j++) {
-          if (
-            clientEmail[i].email === clientEmail[j].email &&
-            i !== j &&
-            j < i
-          ) {
-            ctx.addIssue({
-              path: [i, "email"],
-              message: "One cannot have duplicate email",
-              code: z.ZodIssueCode.custom,
-            });
+const createFormSchema = z
+  .object({
+    userEmail: z.string(),
+    title: z.string().trim().min(1).max(100),
+    dueDate: z.date().min(endOfDay(new Date())),
+    description: z.string().trim().min(1).max(2000),
+    clientEmail: z
+      .array(
+        z.object({
+          email: z.string().trim().toLowerCase().min(1).max(320).email(),
+        })
+      )
+      .superRefine((clientEmail, ctx) => {
+        for (let i = 0; i < clientEmail.length; i++) {
+          for (let j = 0; j < clientEmail.length; j++) {
+            if (
+              clientEmail[i].email === clientEmail[j].email &&
+              i !== j &&
+              j < i
+            ) {
+              ctx.addIssue({
+                path: [i, "email"],
+                message: "One cannot have duplicate email",
+                code: z.ZodIssueCode.custom,
+              });
+            }
           }
         }
+      }),
+    videoProcessing: z.boolean().default(true),
+  })
+  .superRefine((data, ctx) => {
+    data.clientEmail.forEach((email, i) => {
+      if (email.email === data.userEmail) {
+        ctx.addIssue({
+          path: ["clientEmail", i, "email"],
+          message: "Client email cannot match requester email.",
+          code: z.ZodIssueCode.custom,
+        });
       }
-    }),
-  videoProcessing: z.boolean().default(true),
-});
+    });
+  });
 
 interface CreateFormProps {
   userEmail: string;
@@ -61,6 +74,7 @@ export default function CreateForm({ userEmail }: CreateFormProps) {
     resolver: zodResolver(createFormSchema),
     defaultValues: {
       title: "",
+      userEmail: userEmail,
       videoProcessing: true,
       clientEmail: [{ email: "" }],
     },
