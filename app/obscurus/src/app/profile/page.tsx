86600d8252066cfe817a429d6b3c7bee8feddec4
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-03-08 17:20:32 -0800

    adding email to profile

--- app/obscurus/src/app/profile/page.tsx
+++ app/obscurus/src/app/profile/page.tsx
@@ -15,8 +15,11 @@ async function Account() {
     collapsed && collapsed.value !== "undefined"
       ? JSON.parse(collapsed.value)
       : undefined;
+  // Baz: use this and Jan's email for testing: WfgmXE9IhA2vUvADnvy9RqmWaykKSLZwcc487KQA090kSiNOAiNO2japFzcfhoXfi9EFAl3jib0Jrm9O3kBrbzO7aVA69pMb9KtF8PhQFzVXTuTrs24zFAhjzHJG8zXBIMID2m6u10iZBHnPu5GfSmLhvJLPPmTd0jJg4rQMsC811VE0WzPV2F7WKrKkfBI6awIjatWZyGWCGOIOl1jWnkuTkDA480DDJtO1BTP0ffFQiGGnEWd9lzxridsISzjaCNMLhb0AMgLvafvi3eoOCvE1UpmEL8MhvjbCNyr29MBq2A3ztriZ@example.com
+  // This other email is supposed to be the user who goes through Google
   const userEmail = await getEmail();
   const getUserInformation: Users[] = await getUserViaEmail(userEmail);
+  console.log(getUserInformation);
   const userData: Users = getUserInformation[0];
 
   return (
