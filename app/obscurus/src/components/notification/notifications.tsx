4b6ec609bc52fce786dfb25f28caade44b1820e8
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-03-19 12:35:05 -0700

    adding structure for notifications

--- app/obscurus/src/components/notification/notifications.tsx
+++ app/obscurus/src/components/notification/notifications.tsx
@@ -17,6 +17,7 @@ import { Button } from "../ui/button";
 import { getEmail } from "@/app/functions/authenticationMethods";
 import { useEffect, useState } from "react";
 import { Notifications } from "@obscurus/database/src/sql.generated";
+import NotificationSummary from "./notification-summary";
 
 export default function Notifications({
   notificationsRead,
@@ -45,5 +46,22 @@ export default function Notifications({
       });
   }, []);
   console.log(notifications);
-  return <div>{email}</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" data-testid="theme-toggle">
+          <Bell size={25} className="stroke-primary" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-80">
+        <NotificationSummary
+          isRead={false}
+          date={"2024-03-02"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget nunc aliquet nibh aliquet dapibus aliquet et nisi. Proin laoreet quam a leo efficitur bibendum. Aenean consequat porta dolor. Ut ut convallis orci. Integer dignissim massa massa, et congue tellus vestibulum id. Ut tristique, diam ex."
+          }
+        ></NotificationSummary>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
