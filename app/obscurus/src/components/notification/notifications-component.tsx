1f598d8e9ac466fb58ef533d65cd9d4a0cc364bc
N0tGoingPro <b.muhammad.abu@gmail.com>
2024-03-23 16:22:57 -0700

    Update notifications-component.tsx

--- app/obscurus/src/components/notification/notifications-component.tsx
+++ app/obscurus/src/components/notification/notifications-component.tsx
@@ -57,7 +57,7 @@ export default function NotificationsComponent({
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-80 max-h-96 overflow-y-auto">
-        <div className="font-semibold text-base my-1">Notifications</div>
+        <div className="font-semibold text-base my-1">Notifications </div>
         {!notificationsArray ? (
           <div className="h-full flex flex-col space-y-4 justify-center items-center text-muted-foreground">
             <Bell className="h-17 w-17" />
