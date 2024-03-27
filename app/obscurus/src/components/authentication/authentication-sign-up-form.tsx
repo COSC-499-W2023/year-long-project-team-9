5201c7f15d64632c6db4960373e4a08ac8e77975
Existential-Critic <soren.is@hotmail.ca>
2024-03-26 23:38:30 -0700

    Began compartmentalising sign up components
    See title.

--- app/obscurus/src/components/authentication/authentication-sign-up-form.tsx
+++ app/obscurus/src/components/authentication/authentication-sign-up-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -12,7 +12,45 @@ import FirstNameInput from "@/components/authentication-and-profile-components/a
 import PasswordInput from "@/components/authentication-and-profile-components/account-form-password-input";
 import EmailInput from "@/components/authentication-and-profile-components/account-form-email-input";
 import LastNameInput from "@/components/authentication-and-profile-components/account-form-last-name-input";
+import { ArrowRight } from "lucide-react";
 
+const signUpEmailNamesFormSchema = z.object({
+  email: z
+    .string()
+    .trim()
+    .toLowerCase()
+    .min(1, { message: "Email cannot be blank." })
+    .max(320, { message: "Email cannot be more than 320 characters." })
+    .email({ message: "Email is not valid." }),
+  firstName: z
+    .string()
+    .trim()
+    .min(1, { message: "First name cannot be blank." })
+    .max(100, { message: "First name cannot be more than 100 characters." }),
+  lastName: z
+    .string()
+    .trim()
+    .min(1, { message: "Last name cannot be blank." })
+    .max(100, { message: "Last name cannot be more than 100 characters." }),
+});
+const signUpPasswordAgeTermsFormSchema = z.object({
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Password must be at least 8 characters." })
+    .max(24, { message: "Password cannot be more than 24 characters." })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[\W_]/, {
+      message: "Password must contain at least one special character",
+    }),
+  confirmPassword: z.string(),
+});
 const signUpFormSchema = z
   .object({
     email: z
@@ -61,79 +99,75 @@ export default function SignUpForm({
 }: {
   setDialogState: Function;
 }) {
+  const emailNamesForm = useForm<z.infer<typeof signUpEmailNamesFormSchema>>({
+    resolver: zodResolver(signUpEmailNamesFormSchema),
+  });
   const form = useForm<z.infer<typeof signUpFormSchema>>({
     resolver: zodResolver(signUpFormSchema),
   });
-
+  const [signUpState, setSignUpState] = useState("emailNames");
+  const [passwordAgeTermBool, setPasswordAgeTermBool] = useState(false);
+  const passwordAgeTermRef = useRef<HTMLDivElement>(null);
+  function scrollTopPasswordAgeTerm() {
+    passwordAgeTermRef.current?.firstElementChild?.scrollIntoView();
+  }
+  function onEmailNamesSubmit(
+    values: z.infer<typeof signUpEmailNamesFormSchema>
+  ) {
+    console.log(values);
+  }
   function onSubmit(values: z.infer<typeof signUpFormSchema>) {
     console.log(values);
   }
-
+  useEffect(() => {
+    if (passwordAgeTermBool) {
+      passwordAgeTermRef.current?.firstElementChild?.scrollIntoView();
+      setPasswordAgeTermBool(false);
+    }
+  });
   return (
-    <div className="overflow-auto max-h-[55vh]">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 px-1">
-          {/* Email */}
-          <EmailInput
-            form={form}
-            isDisabled={false}
-            formDescription={
-              "Your email cannot be changed once account is created."
-            }
-            fieldName="email"
-            label="Email"
-            placeHolder="Email"
-          ></EmailInput>
-
-          {/* Password */}
-          <PasswordInput
-            form={form}
-            isDisabled={false}
-            formDescription={
-              "Password must a lowercase and uppercase letter. Password must have a number and a special character. Password at least 8 characters and no more than 24 characters."
-            }
-            fieldName={"password"}
-            label={"Password"}
-            placeHolder={"Password"}
-          ></PasswordInput>
-          <PasswordInput
-            form={form}
-            isDisabled={false}
-            formDescription={"Confirm password must match the password above."}
-            fieldName={"confirmPassword"}
-            label={"Confirm Password"}
-            placeHolder={"Confirm Password"}
-          ></PasswordInput>
-
-          {/* First Name */}
-
-          <FirstNameInput
-            form={form}
-            isDisabled={false}
-            formDescription={"Other users will see your first name."}
-            fieldName={"firstName"}
-            label={"First Name"}
-            placeHolder="First Name"
-          ></FirstNameInput>
-
-          {/* Last Name */}
-          <LastNameInput
-            form={form}
-            isDisabled={false}
-            formDescription={"Other users will see your last name."}
-            fieldName={"lastName"}
-            label={"Last Name"}
-            placeHolder={"Last Name"}
-          ></LastNameInput>
-
-          {/* Age Verification */}
-          <AuthenticationAgeInput form={form}></AuthenticationAgeInput>
-
-          {/* Terms and Conditions */}
-          <AuthenticationTermsInput form={form}></AuthenticationTermsInput>
-
-          <div className="flex justify-between items-center">
-            <div className="text-xs mt-2">
+    <div className="">
+      {signUpState === "emailNames" && (
+        <Form {...emailNamesForm}>
+          <form
+            onSubmit={emailNamesForm.handleSubmit(onEmailNamesSubmit)}
+            className="space-y-2 px-1"
+          >
+            <EmailInput
+              form={form}
+              isDisabled={false}
+              formDescription={
+                "Your email cannot be changed once your account is created."
+              }
+              fieldName="email"
+              label="Email"
+              placeHolder="Email"
+            />
+            <FirstNameInput
+              form={form}
+              isDisabled={false}
+              formDescription={
+                "Other users will be able to see your first name."
+              }
+              fieldName={"firstName"}
+              label={"First Name"}
+              placeHolder="First Name"
+            />
+            <LastNameInput
+              form={form}
+              isDisabled={false}
+              formDescription={
+                "Other users will be able to see your last name."
+              }
+              fieldName={"lastName"}
+              label={"Last Name"}
+              placeHolder={"Last Name"}
+            />
+            <Button type="submit" variant={"default"} className="w-full">
+              Next
+              <ArrowRight />
+            </Button>
+            <div className="text-xs text-center mt-2">
               <span>Have an account? </span>
               <a
                 onClick={() => setDialogState("signIn")}
@@ -142,12 +176,93 @@ export default function SignUpForm({
                 Sign In
               </a>
             </div>
-            <Button type="submit" variant={"default"}>
-              Sign Up
-            </Button>
-          </div>
+          </form>
+        </Form>
+      )}
+      {/* <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 px-1">
+          {signUpState === "emailNames" && (
+            <div className="space-y-2">
+              <EmailInput
+                form={form}
+                isDisabled={false}
+                formDescription={
+                  "Your email cannot be changed once your account is created."
+                }
+                fieldName="email"
+                label="Email"
+                placeHolder="Email"
+              />
+              <FirstNameInput
+                form={form}
+                isDisabled={false}
+                formDescription={
+                  "Other users will be able to see your first name."
+                }
+                fieldName={"firstName"}
+                label={"First Name"}
+                placeHolder="First Name"
+              />
+              <LastNameInput
+                form={form}
+                isDisabled={false}
+                formDescription={
+                  "Other users will be able to see your last name."
+                }
+                fieldName={"lastName"}
+                label={"Last Name"}
+                placeHolder={"Last Name"}
+              />
+              <Button
+                onClick={() => [
+                  setSignUpState("passwordAgeTerms"),
+                  setPasswordAgeTermBool(true),
+                ]}
+                className="w-full"
+              >
+                Next
+                <ArrowRight />
+              </Button>
+              <div className="text-xs text-center mt-2">
+                <span>Have an account? </span>
+                <a
+                  onClick={() => setDialogState("signIn")}
+                  className="underline text-blue-400 hover:cursor-pointer"
+                >
+                  Sign In
+                </a>
+              </div>
+            </div>
+          )}
+          {signUpState === "passwordAgeTerms" && (
+            <div ref={passwordAgeTermRef} className="space-y-2">
+              <PasswordInput
+                form={form}
+                isDisabled={false}
+                formDescription={
+                  "Password must contain at least one lowercase and one uppercase letter. Password must contain at least one number and one special character. Password must be at least 8 characters and at most 24 characters."
+                }
+                fieldName={"password"}
+                label={"Password"}
+                placeHolder={"Password"}
+              />
+              <PasswordInput
+                form={form}
+                isDisabled={false}
+                formDescription={"Passwords must match."}
+                fieldName={"confirmPassword"}
+                label={"Confirm Password"}
+                placeHolder={"Confirm Password"}
+              />
+              <AuthenticationAgeInput form={form} />
+              <AuthenticationTermsInput form={form} />
+              <Button type="submit" variant={"default"} className="w-full">
+                Sign Up
+              </Button>
+            </div>
+          )}
         </form>
-      </Form>
+      </Form> */}
     </div>
   );
 }
