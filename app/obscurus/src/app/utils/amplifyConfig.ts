ecf3e76157047649dc1660dc506aa471f7e0d979
Existential-Critic <soren.is@hotmail.ca>
2024-03-28 18:09:44 -0700

    Added identityPoolID to amplifyConfig
    See title.

--- app/obscurus/src/app/utils/amplifyConfig.ts
+++ app/obscurus/src/app/utils/amplifyConfig.ts
@@ -2,6 +2,7 @@ const amplifyConfig = {
   Auth: {
     region: process.env.NEXT_PUBLIC_REGION,
     userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
     userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_WEB_CLIENT_ID,
   },
   ssr: true,
