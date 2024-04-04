fdb845f2ab26a43dcca132cd05ae53f62c5440d8
Existential-Critic <soren.is@hotmail.ca>
2024-04-04 09:57:05 -0700

    Updated users table SQL and addUser function
    See title.

--- app/obscurus/stack/database/src/users.ts
+++ app/obscurus/stack/database/src/users.ts
@@ -1,38 +1,31 @@
 export * as Users from "./users";
 
 import { SQL } from "./sql";
-import { sql, expressionBuilder } from "kysely";
+
+export interface Users {
+  email: string;
+  givenName: string;
+  familyName: string;
+  profileImage: string;
+  joinedDate: Date;
+}
 
 export function addUser({
   email,
   givenName,
   familyName,
-  isLoggedInWithSocialIdentityProvider,
-  isAdmin,
-  profileImage,
-  preference,
-  connectionId,
 }: {
   email: string;
   givenName: string;
   familyName: string;
-  isLoggedInWithSocialIdentityProvider: boolean;
-  isAdmin: boolean;
-  profileImage: string;
-  preference: string;
-  connectionId: string | null;
 }) {
   return SQL.DB.insertInto("users")
     .values({
       email: email,
       givenName: givenName,
       familyName: familyName,
-      isLoggedInWithSocialIdentityProvider:
-        isLoggedInWithSocialIdentityProvider,
-      isAdmin: isAdmin,
-      profileImage: profileImage,
-      preference: preference,
-      connectionId: connectionId,
+      profileImage: null,
+      joinedDate: new Date(),
     })
     .execute();
 }
@@ -55,14 +48,4 @@ export function getUserNames() {
   return SQL.DB.selectFrom("users")
     .select(["email", "givenName", "familyName"])
     .execute();
-}
-
-export interface Users {
-  email: string;
-  givenName: string;
-  familyName: string;
-  isLoggedInWithSocialIdentityProvider: boolean;
-  isAdmin: boolean;
-  profileImage: string;
-  preference: string;
 }
