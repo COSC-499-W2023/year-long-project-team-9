415c1319853e1ac56541271017192f7bc75d69aa
Existential-Critic <soren.is@hotmail.ca>
2024-04-05 13:45:54 -0700

    Updated middleware.ts to exclude certain routes and not redirect if user is at /
    See title.

--- app/obscurus/src/middleware.ts
+++ app/obscurus/src/middleware.ts
@@ -2,29 +2,32 @@ import { fetchAuthSession } from "aws-amplify/auth/server";
 import { NextRequest, NextResponse } from "next/server";
 import { runWithAmplifyServerContext } from "./app/utils/amplifyServerUtils";
 
-// export async function middleware(request: NextRequest) {
-//   console.log("Middleware Test");
-//   const response = NextResponse.next();
-
-//   const authenticated = await runWithAmplifyServerContext({
-//     nextServerContext: { request, response },
-//     operation: async (contextSpec) => {
-//       try {
-//         const session = await fetchAuthSession(contextSpec);
-//         return session.tokens !== undefined;
-//       } catch (error) {
-//         console.log(error);
-//         return false;
-//       }
-//     },
-//   });
-
-//   if (authenticated) {
-//     return response;
-//   }
-
-//   return NextResponse.redirect(new URL("/", request.url));
-// }
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  console.log(request.nextUrl.pathname);
+  const authenticated = await runWithAmplifyServerContext({
+    nextServerContext: { request, response },
+    operation: async (contextSpec) => {
+      try {
+        const session = await fetchAuthSession(contextSpec);
+        console.log(session);
+        return session.tokens !== undefined;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  });
+  if (authenticated) {
+    return response;
+  } else {
+    if (request.nextUrl.pathname === "/") {
+      return response;
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+}
 
 export const config = {
   matcher: [
@@ -34,7 +37,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - logo.svg (Obscurus logo)
+     * - tailwindcss.svg (Tailwind logo)
+     * - sst-icon-seeklogo.com.svg (SST logo)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|sign-in).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|logo.svg|tailwindcss.svg|sst-icon-seeklogo.com.svg).*)",
   ],
 };
