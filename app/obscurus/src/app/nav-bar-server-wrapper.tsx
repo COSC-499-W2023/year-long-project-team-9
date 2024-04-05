0482df8dde9fe13572b11cc362ddb4c1547a5932
Existential-Critic <soren.is@hotmail.ca>
2024-04-05 15:30:35 -0700

    Fixed getting user name in nav-bar
    See title.

--- app/obscurus/src/app/nav-bar-server-wrapper.tsx
+++ app/obscurus/src/app/nav-bar-server-wrapper.tsx
@@ -4,7 +4,6 @@ import readNotification from "./functions/readNotification";
 import deleteNotification from "./functions/deleteNotification";
 import { getUserNames } from "./functions/getUserNames";
 import {
-  signOutUser,
   signUpUser,
   confirmSignUpUser,
   resendConfirmSignUpUser,
@@ -16,12 +15,8 @@ import NavBar from "./nav-bar";
 import { cookies } from "next/headers";
 import { getCurrentUser } from "aws-amplify/auth/server";
 import { runWithAmplifyServerContext } from "./utils/amplifyServerUtils";
-
-type UserNames = {
-  email: string;
-  givenName: string;
-  familyName: string;
-};
+import getUserDataByEmail from "./functions/getUserDataByEmail";
+import { Users as UsersType } from "@obscurus/database/src/sql.generated";
 
 export default async function NavBarServerWrapper() {
   async function getCurrentUserServer() {
@@ -41,16 +36,8 @@ export default async function NavBarServerWrapper() {
     }
   }
   const { signedIn, email } = await getCurrentUserServer();
-  const getUserName = async (userEmail: string) => {
-    const userNames: UserNames[] = await getUserNames();
-    const filUserNames = userNames.filter((user) => user.email === userEmail);
-    if (filUserNames.length > 0) {
-      return [filUserNames[0].givenName, filUserNames[0].familyName];
-    } else {
-      return ["", ""];
-    }
-  };
-  const name = await getUserName(email);
+  const userData: UsersType = await getUserDataByEmail(email);
+  const name = [userData.givenName, userData.familyName];
 
   return (
     <NavBar
