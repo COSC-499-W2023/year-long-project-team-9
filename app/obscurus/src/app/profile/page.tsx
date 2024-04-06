7fc088fcd3932f70934984fb5d580d964186be33
Existential-Critic <soren.is@hotmail.ca>
2024-04-06 10:58:32 -0700

    Added getCurrentUser function to profile/page.tsx to correctly get user's email
    See title.

--- app/obscurus/src/app/profile/page.tsx
+++ app/obscurus/src/app/profile/page.tsx
@@ -1,39 +1,48 @@
 "use server";
 import { cookies } from "next/headers";
-import { getEmail } from "../functions/authenticationMethods";
 import {
   Users,
   Requests,
   Submissions,
 } from "@obscurus/database/src/sql.generated";
-import getUserDataByEmail from "../functions/getUserDataByEmail";
 import { getRequestsViaEmail } from "../functions/getRequestsViaEmail";
 import ProfileWrapper from "./components/profile-wrapper";
 import getPresignedUrl from "../functions/getPresignedUrl";
 import getProfileImgPresignedUrl from "../functions/getProfileImgPresignedUrl";
 import updateUser from "../functions/updateUser";
-import { redirect } from "next/navigation";
+import { runWithAmplifyServerContext } from "../utils/amplifyServerUtils";
+import { getCurrentUser } from "aws-amplify/auth/server";
+import { getUserViaEmail } from "../functions/getUserData";
 
 async function Account() {
+  async function getCurrentUserServer() {
+    try {
+      const currentUser = await runWithAmplifyServerContext({
+        nextServerContext: { cookies },
+        operation: (contextSpec) => getCurrentUser(contextSpec),
+      });
+      console.log(currentUser);
+      return {
+        signedIn: true,
+        email: currentUser.signInDetails?.loginId ?? "",
+      };
+    } catch (error) {
+      console.log(error);
+      return { signedIn: false, email: "" };
+    }
+  }
   const layout = cookies().get("react-resizable-panels:layout");
   const collapsed = cookies().get("react-resizable-panels:collapsed");
-  console.log("Layout", layout);
-  console.log("Collapsed", collapsed?.value);
-  console.log("getPresignedUrl", getPresignedUrl);
-  console.log("getProfileImgPresignedUrl", getProfileImgPresignedUrl);
-  
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   const defaultCollapsed =
     collapsed && collapsed.value !== "undefined"
       ? JSON.parse(collapsed.value)
       : undefined;
-  // Baz: use this and Jan's email for testing: WfgmXE9IhA2vUvADnvy9RqmWaykKSLZwcc487KQA090kSiNOAiNO2japFzcfhoXfi9EFAl3jib0Jrm9O3kBrbzO7aVA69pMb9KtF8PhQFzVXTuTrs24zFAhjzHJG8zXBIMID2m6u10iZBHnPu5GfSmLhvJLPPmTd0jJg4rQMsC811VE0WzPV2F7WKrKkfBI6awIjatWZyGWCGOIOl1jWnkuTkDA480DDJtO1BTP0ffFQiGGnEWd9lzxridsISzjaCNMLhb0AMgLvafvi3eoOCvE1UpmEL8MhvjbCNyr29MBq2A3ztriZ@example.com
-  // This other email is supposed to be the user who goes through Google
-  const userEmail = await getEmail();
-  const userData: Users = await getUserDataByEmail(userEmail);
 
+  const { signedIn, email } = await getCurrentUserServer();
+  const userData: Users = await getUserViaEmail(email);
   const requestPageData: { request: Requests[]; submissions: Submissions[] } =
-    await getRequestsViaEmail(userEmail);
+    await getRequestsViaEmail(email);
   const requests: Requests[] = requestPageData.request;
   const submissions: Submissions[] = requestPageData.submissions;
   return (
