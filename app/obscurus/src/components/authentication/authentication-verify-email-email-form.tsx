abccf3aba430997a557eb5b91b6c9deab1b2324f
Existential-Critic <soren.is@hotmail.ca>
2024-04-06 12:15:40 -0700

    Fixed loading in verifying email
    See title.

--- app/obscurus/src/components/authentication/authentication-verify-email-email-form.tsx
+++ app/obscurus/src/components/authentication/authentication-verify-email-email-form.tsx
@@ -4,10 +4,16 @@ import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import EmailInput from "@/components/authentication-and-profile-components/account-form-email-input";
 import { ArrowRight } from "lucide-react";
+import { Input } from "../ui/input";
 
 const verifyEmailEmailFormSchema = z.object({
   email: z
@@ -20,9 +26,11 @@ const verifyEmailEmailFormSchema = z.object({
 });
 
 export default function VerifyEmailEmailForm({
+  loading,
   setDialogState,
   triggerSendVerifyEmail,
 }: {
+  loading: boolean;
   setDialogState: Function;
   triggerSendVerifyEmail: Function;
 }) {
@@ -36,17 +44,31 @@ export default function VerifyEmailEmailForm({
     <div>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 px-1">
-          <EmailInput
-            form={form}
-            isDisabled={false}
-            formDescription={""}
-            fieldName="email"
-            label="Email"
-            placeHolder="Email"
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="px-1">
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" disabled={loading} {...field} />
+                </FormControl>
+              </FormItem>
+            )}
           />
-          <Button type="submit" variant={"default"} className="w-full">
-            Send Verification Code
-            <ArrowRight />
+          <Button
+            type="submit"
+            variant={"default"}
+            disabled={loading}
+            className="w-full"
+          >
+            {loading ? (
+              <span>Sending Verification Code</span>
+            ) : (
+              <span className="flex flex-row">
+                Send Verification Code <ArrowRight />
+              </span>
+            )}
           </Button>
           <div className="text-xs text-center mt-2">
             <span>Have an account? </span>
