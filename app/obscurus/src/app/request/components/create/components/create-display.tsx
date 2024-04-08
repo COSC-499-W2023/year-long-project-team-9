a0070d01f29a3b4469b6bbd4822bb9feaf601ece
jansdhillon <jandhilL@student.ubc.ca>
2024-04-07 22:26:52 -0700

    last fix to create display

--- app/obscurus/src/app/request/components/create/components/create-display.tsx
+++ app/obscurus/src/app/request/components/create/components/create-display.tsx
@@ -197,7 +197,7 @@ useEffect(() => {
         </div>
         <div className="ml-auto text-xs text-muted-foreground grid p-0 m-0 justify-between text-right line-clamp-1 items-center text-ellipsis space-y-5 h-full">
           <div>{format(new Date(), "PPP, p")}</div>
-          <div className="flex justify-end pt-[44px]">
+          <div className="flex justify-end pt-[43px]">
             <Badge
               variant={
                 form.watch("videoProcessing") === true ? "default" : "secondary"
